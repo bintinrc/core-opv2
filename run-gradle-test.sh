efbd1116661a4515139b32fc1a1a14646b91fa44
Soewandi <soewandi.w@ninjavan.co>
2016-07-25 17:52:56 +0700

    update run-cradle-test.sh

--- run-gradle-test.sh
+++ run-gradle-test.sh
@@ -3,5 +3,5 @@
 # make sure Xvfb is running in background with screen id 1
 export DISPLAY=:1
 
-/opt/gradle/latest/bin/gradle clean cucumberPrep -Penvironment=qa -Ptags=~@selenium
+/opt/gradle/latest/bin/gradle clean cucumberPrep -Penvironment=qa -Ptags=@selenium
 
