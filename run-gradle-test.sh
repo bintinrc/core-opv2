eb02fba8e3cc71b6523d1bb8eec45af802678e20
Soewandi <soewandi.w@ninjavan.co>
2016-09-06 13:10:56 +0700

    update gradle run

--- run-gradle-test.sh
+++ run-gradle-test.sh
@@ -3,5 +3,5 @@
 # make sure Xvfb is running in background with screen id 1
 export DISPLAY=:1
 
-/opt/gradle clean cucumberPrep -Penvironment=qa -Ptags=@selenium
+/opt/gradle --no-daemon clean cucumberPrep -Penvironment=qa -Ptags=@selenium
 
