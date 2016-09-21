88504270351b8d245a5fdaa5f770bda7111718c5
Soewandi <soewandi.w@ninjavan.co>
2016-09-21 13:32:58 +0700

    add settings for seas

---
+++ run-gradle-test-saas.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# make sure Xvfb is running in background with screen id 1
+export DISPLAY=:1
+
+# fix chrome process hang
+export DBUS_SESSION_BUS_ADDRESS=/dev/null
+
+/opt/gradle --no-daemon clean runCucumber -Penvironment=saas -Ptags=@selenium
