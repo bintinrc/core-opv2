a6b0d2138ac34aea8c9f4c3a70a82be4c70d1217
Ferdinand Kurniadi <ferdinand@ninjavan.sg>
2017-04-22 07:40:11 +0700

    update config

--- run-hiptest-publish-result.sh
+++ run-hiptest-publish-result.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+# reload RVM & hiptest-publisher
+source ~/.rvm/scripts/rvm
+
 hiptest-publisher \
   --config-file hiptest-publisher.config \
   --push build/reports/cucumber-junit/cucumber.xml \
