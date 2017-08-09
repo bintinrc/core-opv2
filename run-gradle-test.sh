00c41ebe15a2812393de5d7891e7c9926c0203b1
Daniel Joi Partogi Hutapea <daniel.hutapea@ninjavan.co>
2017-08-09 15:14:17 +0700

    Change command from "printenv" to "/usr/bin/printenv"

--- run-gradle-test.sh
+++ run-gradle-test.sh
@@ -6,5 +6,6 @@ export DISPLAY=:1
 # fix chrome process hang
 export DBUS_SESSION_BUS_ADDRESS=/dev/null
 
-printenv
+/usr/bin/printenv
+
 gradle --no-daemon clean runCucumberParallel -PforkCount=4 -Penvironment=qa -Ptags=@LoginLogout
