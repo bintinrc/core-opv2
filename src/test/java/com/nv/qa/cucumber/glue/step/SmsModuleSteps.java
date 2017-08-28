121d68e18b9937316ef952f238ea9312f4faf899
Rizaq Nanda Pratama <rizaq.pratama@ninjavan.co>
2017-08-28 11:53:47 +0700

    fix sms scenario

--- src/test/java/com/nv/qa/cucumber/glue/step/SmsModuleSteps.java
+++ src/test/java/com/nv/qa/cucumber/glue/step/SmsModuleSteps.java
@@ -3,10 +3,14 @@ package com.nv.qa.cucumber.glue.step;
 import com.google.inject.Inject;
 import com.nv.qa.model.SmsCampaignCsv;
 import com.nv.qa.selenium.page.SmsModulePage;
+import com.nv.qa.support.ScenarioStorage;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.nv.qa.model.order_creation.v2.Order;
 
 /**
  * Created by rizaqpratama on 7/19/17.
@@ -15,18 +19,31 @@ public class SmsModuleSteps extends AbstractSteps
 {
 
     private SmsModulePage smsModulePage;
+    private final ScenarioStorage scenarioStorage;
     @Override
     public void init() {
         smsModulePage = new SmsModulePage(getDriver());
     }
 
     @Inject
-    public SmsModuleSteps(CommonScenario commonScenario){
+    public SmsModuleSteps(CommonScenario commonScenario, ScenarioStorage scenarioStorage){
         super(commonScenario);
+        this.scenarioStorage = scenarioStorage;
     }
 
     @Then("^op upload sms campaign csv file")
     public void uploadSmsCampaignCsv(List<SmsCampaignCsv> data){
+        Order order = scenarioStorage.get("order");
+        if(order != null ){
+            String trackingId = order.getTracking_id();
+            data = data.stream().map(smsCampaignCsv -> {
+                if(smsCampaignCsv.getTracking_id().equalsIgnoreCase("_created_")){
+                    smsCampaignCsv.setTracking_id(trackingId);
+                }
+                return smsCampaignCsv;
+            }).collect(Collectors.toList());
+        }
+
         smsModulePage.uploadCsvCampaignFile(data);
     }
 
@@ -42,6 +59,13 @@ public class SmsModuleSteps extends AbstractSteps
 
     @When("^op compose sms with data : ([^\"]*), ([^\"]*)")
     public void composeSms(String name, String trackingId){
+        Order order = scenarioStorage.get("order");
+        if(order != null){
+            String nTrackingId = order.getTracking_id();
+            if(trackingId.equalsIgnoreCase("_created_")){
+                trackingId = nTrackingId;
+            }
+        }
         smsModulePage.composeSms(name, trackingId);
     }
 
@@ -78,6 +102,10 @@ public class SmsModuleSteps extends AbstractSteps
 
     @Then("^op verify that sms sent to phone number ([^\"]*) and tracking id ([^\"]*)")
     public void verifyOnTrackingIdValid(String trackingId, String contactNumber){
+        Order order = scenarioStorage.get("order");
+        if(order != null && trackingId.equalsIgnoreCase("_created_")){
+            trackingId = order.getTracking_id();
+        }
         smsModulePage.searchSmsSentHistory(trackingId);
         smsModulePage.verifySmsHistoryTrackingIdValid(trackingId, contactNumber);
     }
