343516a26b113a38b4bc7edc33c81fe005abcd9b
Daniel Joi Partogi Hutapea <daniel.hutapea@ninjavan.co>
2017-10-11 10:43:32 +0700

    Add code to retry if StaleElementException is occured.

--- src/test/java/com/nv/qa/cucumber/glue/step/BlockedDatesStep.java
+++ src/test/java/com/nv/qa/cucumber/glue/step/BlockedDatesStep.java
@@ -1,14 +1,14 @@
 package com.nv.qa.cucumber.glue.step;
 
+import com.google.inject.Inject;
 import com.nv.qa.support.CommonUtil;
 import com.nv.qa.support.ScenarioHelper;
-import com.nv.qa.support.SeleniumSharedDriver;
-import cucumber.api.java.Before;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import cucumber.runtime.java.guice.ScenarioScoped;
 import org.junit.Assert;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -17,108 +17,149 @@ import java.util.List;
 import java.util.Locale;
 
 /**
- * Created by sw on 8/19/16.
+ *
+ * @author Soewandi Wirjawan
  */
 @ScenarioScoped
-public class BlockedDatesStep {
+public class BlockedDatesStep  extends AbstractSteps
+{
+    private static final SimpleDateFormat MONTH_SDF = new SimpleDateFormat("MMMM", Locale.ENGLISH);
 
-    private WebDriver driver;
+    @Inject
+    public BlockedDatesStep(CommonScenario commonScenario)
+    {
+        super(commonScenario);
+    }
 
-    @Before
-    public void setup() {
-        driver = SeleniumSharedDriver.getInstance().getDriver();
+    @Override
+    public void init()
+    {
     }
 
     @When("^blocked dates add$")
-    public void add() {
-
+    public void add()
+    {
         /**
          * Set default year of "Blocked Dates" on right panel to current year.
          */
-        WebElement blockedDatesYearWe = driver.findElement(By.xpath("//div[contains(@class, 'list')]/md-content[contains(@class, 'list-content')]/div/md-input-container"));
+        WebElement blockedDatesYearWe = getDriver().findElement(By.xpath("//div[contains(@class, 'list')]/md-content[contains(@class, 'list-content')]/div/md-input-container"));
         blockedDatesYearWe.click();
 
         int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        WebElement currentYearOptionWe = driver.findElement(By.xpath(String.format("//md-option[@ng-repeat='m in yearList' and @value='%d']", currentYear)));
+        WebElement currentYearOptionWe = getDriver().findElement(By.xpath(String.format("//md-option[@ng-repeat='m in yearList' and @value='%d']", currentYear)));
         currentYearOptionWe.click();
 
-        List<WebElement> elm = driver.findElements(By.xpath("//div[@ng-repeat='day in week track by $index' and not(contains(@class, 'active')) and not(contains(@class, 'not-same-month'))]"));
-        if (!elm.isEmpty()) {
+        List<WebElement> elm = getDriver().findElements(By.xpath("//div[@ng-repeat='day in week track by $index' and not(contains(@class, 'active')) and not(contains(@class, 'not-same-month'))]"));
+
+        if(!elm.isEmpty())
+        {
             WebElement day = elm.get(0);
             day.click();
 
-            WebElement yearElm = driver.findElement(By.xpath("//md-select[@ng-model='calendar.year']/md-select-value/span/div"));
+            WebElement yearElm = getDriver().findElement(By.xpath("//md-select[@ng-model='calendar.year']/md-select-value/span/div"));
             ScenarioHelper.getInstance().setTmpId(yearElm.getText() + "-" + getMonth() + "-" + getDay(day));
-            CommonUtil.clickBtn(driver, "//button[@type='submit'][@aria-label='Save Button']");
+            CommonUtil.clickBtn(getDriver(), "//button[@type='submit'][@aria-label='Save Button']");
         }
     }
 
     @Then("^blocked dates verify add$")
-    public void verifyAdd() {
-        if (ScenarioHelper.getInstance().getTmpId() != null) {
-            boolean isAdded = false;
-            List<WebElement> els = driver.findElements(By.xpath("//md-list-item[@ng-repeat=\"date in calendarListData.dates | nvFilterByYear:year | orderBy\"]/p/span[1]"));
-            for (WebElement el : els) {
-                if (el.getText().contains(ScenarioHelper.getInstance().getTmpId())) {
-                    isAdded = true;
-                    break;
+    public void verifyAdd()
+    {
+        if(ScenarioHelper.getInstance().getTmpId()!=null)
+        {
+            CommonUtil.retryIfStaleElementReferenceExceptionOccurred(() ->
+            {
+                boolean isAdded = false;
+                List<WebElement> els = getDriver().findElements(By.xpath("//md-list-item[@ng-repeat=\"date in calendarListData.dates | nvFilterByYear:year | orderBy\"]/p/span[1]"));
+
+                for(WebElement el : els)
+                {
+                    if(el.getText().contains(ScenarioHelper.getInstance().getTmpId()))
+                    {
+                        isAdded = true;
+                        break;
+                    }
                 }
-            }
-            Assert.assertTrue(isAdded);
+
+                Assert.assertTrue(isAdded);
+            });
         }
     }
 
     @When("^blocked dates remove$")
-    public void remove() {
-        if (ScenarioHelper.getInstance().getTmpId() != null) {
+    public void remove()
+    {
+        if (ScenarioHelper.getInstance().getTmpId()!=null)
+        {
             boolean isRemoved = false;
-            List<WebElement> els = driver.findElements(By.xpath("//md-list-item[@ng-repeat=\"date in calendarListData.dates | nvFilterByYear:year | orderBy\"]"));
-            for (WebElement el : els) {
+            List<WebElement> els = getDriver().findElements(By.xpath("//md-list-item[@ng-repeat=\"date in calendarListData.dates | nvFilterByYear:year | orderBy\"]"));
+
+            for(WebElement el : els)
+            {
                 WebElement inner = el.findElement(By.xpath("p/span[1]"));
-                if (inner.getText().contains(ScenarioHelper.getInstance().getTmpId())) {
+
+                if(inner.getText().contains(ScenarioHelper.getInstance().getTmpId()))
+                {
                     el.findElement(By.xpath("button[@ng-click=\"removeDate(date, $event)\"]")).click();
-                    CommonUtil.clickBtn(driver, "//button[@type='submit'][@aria-label='Save Button']");
+                    CommonUtil.clickBtn(getDriver(), "//button[@type='submit'][@aria-label='Save Button']");
                     isRemoved = true;
                     break;
                 }
             }
+
             Assert.assertTrue(isRemoved);
         }
 
     }
 
     @Then("^blocked dates verify remove$")
-    public void verifyRemove() {
-        if (ScenarioHelper.getInstance().getTmpId() != null) {
-            boolean isFound = false;
-            List<WebElement> els = driver.findElements(By.xpath("//md-list-item[@ng-repeat=\"date in calendarListData.dates | nvFilterByYear:year | orderBy\"]/p/span[1]"));
-            for (WebElement el : els) {
-                if (el.getText().contains(ScenarioHelper.getInstance().getTmpId())) {
-                    isFound = true;
-                    break;
+    public void verifyRemove()
+    {
+        if(ScenarioHelper.getInstance().getTmpId()!=null)
+        {
+            CommonUtil.retryIfStaleElementReferenceExceptionOccurred(() ->
+            {
+                boolean isFound = false;
+                List<WebElement> els = getDriver().findElements(By.xpath("//md-list-item[@ng-repeat=\"date in calendarListData.dates | nvFilterByYear:year | orderBy\"]/p/span[1]"));
+
+                for(WebElement el : els)
+                {
+                    if(el.getText().contains(ScenarioHelper.getInstance().getTmpId()))
+                    {
+                        isFound = true;
+                        break;
+                    }
                 }
-            }
-            Assert.assertFalse(isFound);
+
+                Assert.assertFalse(isFound);
+            });
         }
     }
 
-    private String getDay(WebElement day) {
+    private String getDay(WebElement day)
+    {
         WebElement dayTxt = day.findElement(By.xpath("div[1]"));
         int dayNumber = Integer.parseInt(dayTxt.getText());
         return dayNumber < 10 ? "0" + dayNumber : "" + dayNumber;
     }
 
-    private String getMonth() {
+    private String getMonth()
+    {
         String monthText = null;
-        try {
-            WebElement monthElm = driver.findElement(By.xpath("//md-select[@ng-model='calendar.month']/md-select-value/span/div"));
-            Date date = new SimpleDateFormat("MMMM", Locale.ENGLISH).parse(monthElm.getText());
+
+        try
+        {
+            WebElement monthElm = getDriver().findElement(By.xpath("//md-select[@ng-model='calendar.month']/md-select-value/span/div"));
+            Date date = MONTH_SDF.parse(monthElm.getText());
             Calendar cal = Calendar.getInstance();
             cal.setTime(date);
             int month = cal.get(Calendar.MONTH) + 1;
             monthText = month < 10 ? "0" + month : "" + month;
-        } catch (Exception e) {
         }
+        catch(Exception e)
+        {
+        }
+
         return monthText;
     }
 }
--- src/test/resources/cucumber/feature/blocked_dates.feature
+++ src/test/resources/cucumber/feature/blocked_dates.feature
@@ -1,11 +1,9 @@
 @BlockedDates @selenium @saas
 Feature: Blocked Dates
 
-  Scenario: op login into operator portal
-    Given op is in op portal login page
-    When login button is clicked
-    When login as "{operator-portal-uid}" with password "{operator-portal-pwd}"
-    Then op is in main page
+  @LaunchBrowser
+  Scenario: Login to Operator V2
+    Given op login into Operator V2 with username "{operator-portal-uid}" and password "{operator-portal-pwd}"
 
   # add blocked dates
   Scenario: add blocked date (uid:0877c2fd-f75e-4f90-96a8-8ce8da082009)
@@ -19,5 +17,5 @@ Feature: Blocked Dates
     When blocked dates remove
     Then blocked dates verify remove
 
-  @closeBrowser
-  Scenario: close browser
+  @KillBrowser
+  Scenario: Kill Browser
