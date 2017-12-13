c26470dbc14d5b6e7b28d7fbed329127b9201a5e
Daniel Joi Partogi Hutapea <daniel.hutapea@ninjavan.co>
2017-12-13 11:55:57 +0700

    Cleaning Constants code.

--- src/test/java/co/nvqa/operator_v2/util/TestConstants.java
+++ src/test/java/co/nvqa/operator_v2/util/TestConstants.java
@@ -3,19 +3,14 @@ package co.nvqa.operator_v2.util;
 import co.nvqa.commons.utils.NvLogger;
 import co.nvqa.commons.utils.StandardTestConstants;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-
 /**
  *
  * @author Soewandi Wirjawan
  */
 public final class TestConstants extends StandardTestConstants
 {
-    private static final SimpleDateFormat TEMP_FOLDER_DATE_FORMAT = new SimpleDateFormat("dd-MMM-yyyy_hhmmss_SSS");
-
-    private static final String CONFIGURATION_FILE = "config.properties";
     private static final String ENVIRONMENT_SYSTEM_PROPERTY = "environment";
+    private static final String CONFIGURATION_FILE = "config.properties";
 
     public static final boolean ENABLE_PROXY;
     public static final long PROXY_READ_BANDWIDTH_LIMIT_IN_BPS;
@@ -23,7 +18,6 @@ public final class TestConstants extends StandardTestConstants
 
     public static final String SELENIUM_DRIVER;
     public static final String SELENIUM_CHROME_DRIVER;
-    public static final String SELENIUM_CHROME_BINARY_PATH;
     public static final int SELENIUM_IMPLICIT_WAIT_TIMEOUT_SECONDS;
     public static final int SELENIUM_PAGE_LOAD_TIMEOUT_SECONDS;
     public static final int SELENIUM_SCRIPT_TIMEOUT_SECONDS;
@@ -54,63 +48,45 @@ public final class TestConstants extends StandardTestConstants
 
     static
     {
-        try
-        {
-            loadProperties(ENVIRONMENT_SYSTEM_PROPERTY, CONFIGURATION_FILE);
+        loadProperties(ENVIRONMENT_SYSTEM_PROPERTY, CONFIGURATION_FILE);
 
-            ENABLE_PROXY = getPropertyValueAsBoolean("enable-proxy");
-            PROXY_READ_BANDWIDTH_LIMIT_IN_BPS = getPropertyValueAsLong("proxy-read-bandwidth-limit-in-bps");
-            PROXY_WRITE_BANDWIDTH_LIMIT_IN_BPS = getPropertyValueAsLong("proxy-write-bandwidth-limit-in-bps");
+        ENABLE_PROXY = getPropertyValueAsBoolean("enable-proxy");
+        PROXY_READ_BANDWIDTH_LIMIT_IN_BPS = getPropertyValueAsLong("proxy-read-bandwidth-limit-in-bps");
+        PROXY_WRITE_BANDWIDTH_LIMIT_IN_BPS = getPropertyValueAsLong("proxy-write-bandwidth-limit-in-bps");
 
-            SELENIUM_DRIVER = getPropertyValueAsString("selenium-driver");
-            SELENIUM_CHROME_DRIVER = getPropertyValueAsString("selenium-chrome-driver-location");
+        SELENIUM_DRIVER = getPropertyValueAsString("selenium-driver");
+        SELENIUM_CHROME_DRIVER = getPropertyValueAsString("selenium-chrome-driver-location");
 
-            String seleniumChromeBinaryPathTemp = getPropertyValueAsString("selenium-chrome_binary-path");
+        SELENIUM_IMPLICIT_WAIT_TIMEOUT_SECONDS = getPropertyValueAsInteger("selenium-implicit-wait-timeout-seconds");
+        SELENIUM_PAGE_LOAD_TIMEOUT_SECONDS = getPropertyValueAsInteger("selenium-page-load-timeout-seconds");
+        SELENIUM_SCRIPT_TIMEOUT_SECONDS = getPropertyValueAsInteger("selenium-script-timeout-seconds");
+        SELENIUM_DEFAULT_WEB_DRIVER_WAIT_TIMEOUT_IN_SECONDS = getPropertyValueAsLong("selenium-default-web-driver-wait-timeout-in-seconds");
+        SELENIUM_WINDOW_WIDTH = getPropertyValueAsInteger("selenium-window-width");
+        SELENIUM_WINDOW_HEIGHT = getPropertyValueAsInteger("selenium-window-height");
+        SELENIUM_WINDOW_POSITION_X = getPropertyValueAsInteger("selenium-window-position-x");
+        SELENIUM_WINDOW_POSITION_Y = getPropertyValueAsInteger("selenium-window-position-y");
 
-            if(seleniumChromeBinaryPathTemp==null || seleniumChromeBinaryPathTemp.isEmpty())
-            {
-                seleniumChromeBinaryPathTemp = System.getenv("bamboo_capability_system_builder_command_google_chrome");
-                NvLogger.info("Using SELENIUM_CHROME_BINARY_PATH from \"System Environment\" variable. [bamboo_capability_system_builder_command_google_chrome = '"+seleniumChromeBinaryPathTemp+"']");
-            }
+        OPERATOR_PORTAL_URL = getPropertyValueAsString("operator-portal-url");
+        OPERATOR_PORTAL_UID = getPropertyValueAsString("operator-portal-uid");
+        OPERATOR_PORTAL_PWD = getPropertyValueAsString("operator-portal-pwd");
 
-            SELENIUM_CHROME_BINARY_PATH = seleniumChromeBinaryPathTemp;
-            NvLogger.info("Selenium Chrome Binary Path = "+SELENIUM_CHROME_BINARY_PATH);
+        OPERATOR_PORTAL_FORCE_LOGIN_BY_INJECTING_COOKIES = getPropertyValueAsBoolean("operator-portal-force-login-by-injecting-cookies");
+        OPERATOR_PORTAL_USER_COOKIE = getPropertyValueAsString("operator-portal-user-cookie");
 
-            SELENIUM_IMPLICIT_WAIT_TIMEOUT_SECONDS = getPropertyValueAsInteger("selenium-implicit-wait-timeout-seconds");
-            SELENIUM_PAGE_LOAD_TIMEOUT_SECONDS = getPropertyValueAsInteger("selenium-page-load-timeout-seconds");
-            SELENIUM_SCRIPT_TIMEOUT_SECONDS = getPropertyValueAsInteger("selenium-script-timeout-seconds");
-            SELENIUM_DEFAULT_WEB_DRIVER_WAIT_TIMEOUT_IN_SECONDS = getPropertyValueAsLong("selenium-default-web-driver-wait-timeout-in-seconds");
-            SELENIUM_WINDOW_WIDTH = getPropertyValueAsInteger("selenium-window-width");
-            SELENIUM_WINDOW_HEIGHT = getPropertyValueAsInteger("selenium-window-height");
-            SELENIUM_WINDOW_POSITION_X = getPropertyValueAsInteger("selenium-window-position-x");
-            SELENIUM_WINDOW_POSITION_Y = getPropertyValueAsInteger("selenium-window-position-y");
+        API_BASE_URL = getPropertyValueAsString("api-base-url");
 
-            OPERATOR_PORTAL_URL = getPropertyValueAsString("operator-portal-url");
-            OPERATOR_PORTAL_UID = getPropertyValueAsString("operator-portal-uid");
-            OPERATOR_PORTAL_PWD = getPropertyValueAsString("operator-portal-pwd");
+        SHIPPER_V3_ID = getPropertyValueAsInteger("shipper-v3-id");
+        SHIPPER_V3_CLIENT_ID = getPropertyValueAsString("shipper-v3-client-id");
+        SHIPPER_V3_CLIENT_SECRET = getPropertyValueAsString("shipper-v3-client-secret");
 
-            OPERATOR_PORTAL_FORCE_LOGIN_BY_INJECTING_COOKIES = getPropertyValueAsBoolean("operator-portal-force-login-by-injecting-cookies");
-            OPERATOR_PORTAL_USER_COOKIE = getPropertyValueAsString("operator-portal-user-cookie");
+        SHIPPER_V2_ID = getPropertyValueAsInteger("shipper-v2-id");
+        SHIPPER_V2_NAME = getPropertyValueAsString("shipper-v2-name");
+        SHIPPER_V2_CLIENT_ID = getPropertyValueAsString("shipper-v2-client-id");
+        SHIPPER_V2_CLIENT_SECRET = getPropertyValueAsString("shipper-v2-client-secret");
 
-            API_BASE_URL = getPropertyValueAsString("api-base-url");
-
-            SHIPPER_V3_ID = getPropertyValueAsInteger("shipper-v3-id");
-            SHIPPER_V3_CLIENT_ID = getPropertyValueAsString("shipper-v3-client-id");
-            SHIPPER_V3_CLIENT_SECRET = getPropertyValueAsString("shipper-v3-client-secret");
-
-            SHIPPER_V2_ID = getPropertyValueAsInteger("shipper-v2-id");
-            SHIPPER_V2_NAME = getPropertyValueAsString("shipper-v2-name");
-            SHIPPER_V2_CLIENT_ID = getPropertyValueAsString("shipper-v2-client-id");
-            SHIPPER_V2_CLIENT_SECRET = getPropertyValueAsString("shipper-v2-client-secret");
-
-            NINJA_DRIVER_ID = getPropertyValueAsInteger("ninja-driver-id");
-            NINJA_DRIVER_USERNAME = getPropertyValueAsString("ninja-driver-username");
-            NINJA_DRIVER_PASSWORD = getPropertyValueAsString("ninja-driver-password");
-        }
-        catch(IOException ex)
-        {
-            throw new RuntimeException("Fail to load file properties.", ex);
-        }
+        NINJA_DRIVER_ID = getPropertyValueAsInteger("ninja-driver-id");
+        NINJA_DRIVER_USERNAME = getPropertyValueAsString("ninja-driver-username");
+        NINJA_DRIVER_PASSWORD = getPropertyValueAsString("ninja-driver-password");
     }
 
     private TestConstants()
