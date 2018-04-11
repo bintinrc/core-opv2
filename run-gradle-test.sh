3c10315942ce9ff37564eba733cf4bcf0105caa8
Daniel Joi Partogi Hutapea <daniel.hutapea@ninjavan.co>
2018-04-11 15:32:41 +0700

    Add "set -o errexit" to the script.

--- run-gradle-test.sh
+++ run-gradle-test.sh
@@ -14,7 +14,10 @@ _term() {
 trap _term SIGTERM
 
 echo "Doing some initial work...";
+set -o errexit
 gradle --no-daemon clean runCucumberParallel -PforkCount=4 -Penvironment=qa -Ptags=@OperatorV2 &
 
 child=$!
+echo "Wait until Gradle done..."
 wait "$child"
+echo "Gradle is done."
