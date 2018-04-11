9db81d9912cafd21cdf3a33d937b80dcac96eb67
Daniel Joi Partogi Hutapea <daniel.hutapea@ninjavan.co>
2018-04-11 14:49:50 +0700

    - Add code to cleaning shipper's address and put it on a feature. - Add code to run-gradle-test.sh to trap SIGTERM from bamboo.

--- run-gradle-test.sh
+++ run-gradle-test.sh
@@ -6,4 +6,15 @@ export DISPLAY=:1
 # fix chrome process hang
 export DBUS_SESSION_BUS_ADDRESS=/dev/null
 
-gradle --no-daemon clean runCucumberParallel -PforkCount=4 -Penvironment=qa -Ptags=@OperatorV2
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+
+echo "Doing some initial work...";
+gradle --no-daemon clean runCucumberParallel -PforkCount=4 -Penvironment=qa -Ptags=@OperatorV2 &
+
+child=$!
+wait "$child"
---
+++ src/test/resources/cucumber/feature/00_cleaning.feature
@@ -0,0 +1,5 @@
+@OperatorV2 @Cleaning
+Feature: Cleaning
+
+  Scenario: Clean shipper's address
+    Given API Operator clean address of shipper with ID = "{shipper-v2-id}"
