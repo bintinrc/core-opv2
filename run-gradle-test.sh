5827cbb36655ea1dcb6fc585ea4014d5cd6676b1
Daniel Joi Partogi Hutapea <daniel.hutapea@ninjavan.co>
2018-04-11 15:41:47 +0700

    Update run-gradle-test.

--- run-gradle-test.sh
+++ run-gradle-test.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+set -o errexit
 
 # make sure Xvfb is running in background with screen id 1
 export DISPLAY=:1
@@ -6,18 +7,5 @@ export DISPLAY=:1
 # fix chrome process hang
 export DBUS_SESSION_BUS_ADDRESS=/dev/null
 
-_term() {
-  echo "Caught SIGTERM signal!"
-  kill -TERM "$child" 2>/dev/null
-}
-
-trap _term SIGTERM
-
 echo "Doing some initial work...";
-set -o errexit
-gradle --no-daemon clean runCucumberParallel -PforkCount=4 -Penvironment=qa -Ptags=@TagManagement &
-
-child=$!
-echo "Wait until Gradle done..."
-wait "$child"
-echo "Gradle is done."
+gradle --no-daemon clean runCucumberParallel -PforkCount=4 -Penvironment=qa -Ptags=@TagManagement
