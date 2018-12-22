8a7c4f00b656a1ef97c37447880e4b653982d2ec
ssmishanin <sergey.mishanin1985@gmail.com>
2018-12-23 06:02:46 +0800

    new scenarios

--- src/test/java/co/nvqa/operator_v2/selenium/page/CreateRouteGroupsPage.java
+++ src/test/java/co/nvqa/operator_v2/selenium/page/CreateRouteGroupsPage.java
@@ -1,10 +1,20 @@
 package co.nvqa.operator_v2.selenium.page;
 
+import co.nvqa.operator_v2.model.TxnRsvn;
 import co.nvqa.operator_v2.util.TestUtils;
+import com.google.common.collect.ImmutableMap;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import static co.nvqa.operator_v2.selenium.page.CreateRouteGroupsPage.TxnRsvnTable.COLUMN_TRACKING_ID;
+import static co.nvqa.operator_v2.selenium.page.CreateRouteGroupsPage.TxnRsvnTable.COLUMN_TYPE;
 
 /**
  * @author Daniel Joi Partogi Hutapea
@@ -12,10 +22,22 @@ import java.text.SimpleDateFormat;
 public class CreateRouteGroupsPage extends OperatorV2SimplePage
 {
     private static final SimpleDateFormat DATE_FILTER_SDF = new SimpleDateFormat("EEEE MMMM d yyyy");
+    private static final SimpleDateFormat DATE_FILTER_SDF_2 = new SimpleDateFormat("yyyy-MM-dd");
+    private static final String XPATH_GENERAL_FILTERS = "//div[@possible-filters='ctrl.possibleGeneralFilters']";
+    private static final String XPATH_TRANSACTION_FILTERS = "//div[@possible-filters='ctrl.possibleTxnFilters']";
+    private static final String XPATH_FILTER_BY_TITLE = "//div[@class='filter-container'][.//div[contains(@class,'main-title')]/p[.='%s']]";
+
+    private Map<String, Consumer<String>> filterSetters;
+    private TxnRsvnTable txnRsvnTable;
 
     public CreateRouteGroupsPage(WebDriver webDriver)
     {
         super(webDriver);
+        filterSetters = ImmutableMap.of(
+                "RTS", value -> setSwitchFilter(XPATH_TRANSACTION_FILTERS, "RTS", value),
+                "Routed", value -> setSwitchFilter(XPATH_GENERAL_FILTERS, "Routed", value),
+                "Creation Time", this::setCreationTimeFilter);
+        txnRsvnTable = new TxnRsvnTable(webDriver);
     }
 
     public void waitUntilRouteGroupPageIsLoaded()
@@ -57,19 +79,110 @@ public class CreateRouteGroupsPage extends OperatorV2SimplePage
         pause500ms();
     }
 
+    public void setCreationTimeFilter(String dates)
+    {
+        String fromDateStr;
+        String toDateStr;
+        if (StringUtils.equalsIgnoreCase("today", dates))
+        {
+            fromDateStr = DATE_FILTER_SDF_2.format(new Date());
+            toDateStr = fromDateStr;
+        } else
+        {
+            String[] datesStr = dates.split(";");
+            fromDateStr = StringUtils.normalizeSpace(datesStr[0]);
+            toDateStr = StringUtils.normalizeSpace(datesStr[1]);
+        }
+
+        /*
+          Set fromHour & fromMinute of Creation Time.
+         */
+        sendKeys("//md-datepicker[@name='fromDateField']//input", fromDateStr);
+        pause500ms();
+        click("//md-input-container[@model='container.fromHour']");
+        pause500ms();
+        click("//div[@aria-hidden='false']/md-select-menu/md-content/md-option/div[contains(text(), ' 00 ')]");
+        pause500ms();
+        click("//md-input-container[@model='container.fromMinute']");
+        pause500ms();
+        click("//div[@aria-hidden='false']/md-select-menu/md-content/md-option/div[contains(text(), ' 00 ')]");
+        pause500ms();
+
+        /*
+          Set toHour & toMinute of Creation Time.
+         */
+        sendKeys("//md-datepicker[@name='toDateField']//input", toDateStr);
+        pause500ms();
+        click("//md-input-container[@model='container.toHour']");
+        pause500ms();
+        click("//div[@aria-hidden='false']/md-select-menu/md-content/md-option/div[contains(text(), ' 23 ')]");
+        pause500ms();
+        click("//md-input-container[@model='container.toMinute']");
+        pause500ms();
+        click("//div[@aria-hidden='false']/md-select-menu/md-content/md-option/div[contains(text(), ' 30 ')]");
+        pause500ms();
+    }
+
     public void removeFilter(String filterName)
     {
-        if(filterName.contains("time"))
+        if (filterName.contains("time"))
         {
-            if(isElementExist("//div[div[p[text()='" + filterName + "']]]/div/nv-icon-button/button"))
+            if (isElementExist("//div[div[p[text()='" + filterName + "']]]/div/nv-icon-button/button"))
             {
                 click("//div[div[p[text()='" + filterName + "']]]/div/nv-icon-button/button");
             }
-        }
-        else
+        } else
         {
             click("//div[div[p[text()='" + filterName + "']]]/div/div/nv-icon-button/button");
         }
+    }
+
+    public void removeAllFilterExceptGiven(List<String> filters)
+    {
+        String xpath = "//div[@class='filter-container']//div[contains(@class,'main-title')]/p";
+        List<String> filterTitles = getTextOfElements(xpath);
+        filterTitles.removeAll(filters);
+        String removeFilterXpath = XPATH_FILTER_BY_TITLE + "//*[@name='commons.remove-filter']";
+        filterTitles.forEach(filter -> clickf(removeFilterXpath, filter));
+    }
+
+    public void selectTransactionFiltersMode(String value)
+    {
+        clickf("//div[@model='ctrl.includeTxnModel']//button[@aria-label='%s']", value);
+    }
+
+    public void selectReservationFiltersMode(String value)
+    {
+        clickf("//div[@model='ctrl.includeRxnModel']//button[@aria-label='%s']", value);
+    }
+
+    public void addTransactionFilters(Map<String, String> data)
+    {
+        data.forEach((filter, value) -> {
+            String xpath = String.format(XPATH_TRANSACTION_FILTERS + XPATH_FILTER_BY_TITLE, filter);
+            if (!isElementExistFast(xpath))
+            {
+                executeInContext(XPATH_TRANSACTION_FILTERS, () -> selectValueFromNvAutocompleteBySearchTextAndDismiss("::searchText", filter));
+            }
+            filterSetters.get(filter).accept(value);
+        });
+    }
+
+    public void addGeneralFilters(Map<String, String> data)
+    {
+        data.forEach((filter, value) -> {
+            String xpath = String.format(XPATH_GENERAL_FILTERS + XPATH_FILTER_BY_TITLE, filter);
+            if (!isElementExistFast(xpath))
+            {
+                executeInContext(XPATH_GENERAL_FILTERS, () -> selectValueFromNvAutocompleteBySearchTextAndDismiss("::searchText", filter));
+            }
+            filterSetters.get(filter).accept(value);
+        });
+    }
+
+    public void setSwitchFilter(String blockXpath, String filter, String value)
+    {
+        clickf(blockXpath + XPATH_FILTER_BY_TITLE + "//button[@aria-label='%s']", filter, value);
     }
 
     public void clickButtonLoadSelection()
@@ -108,5 +221,47 @@ public class CreateRouteGroupsPage extends OperatorV2SimplePage
     {
         clickNvButtonSaveByNameAndWaitUntilDone("Add Transactions/Reservations");
         waitUntilInvisibilityOfToast("Added successfully");
+    }
+
+    public void validateRecord(TxnRsvn expectedRecord, String type)
+    {
+        txnRsvnTable.filterByColumn(COLUMN_TRACKING_ID, expectedRecord.getTrackingId());
+        txnRsvnTable.filterByColumn(COLUMN_TYPE, type);
+        TxnRsvn actualRecord = txnRsvnTable.readEntity(1);
+        expectedRecord.compareWithActual(actualRecord);
+    }
+
+    /**
+     * Accessor for Transaction/Reservation table
+     */
+    public static class TxnRsvnTable extends MdVirtualRepeatTable<TxnRsvn>
+    {
+        public static final String MD_VIRTUAL_REPEAT = "trvn in getTableData()";
+        public static final String COLUMN_TRACKING_ID = "trackingId";
+        public static final String COLUMN_TYPE = "type";
+
+        public TxnRsvnTable(WebDriver webDriver)
+        {
+            super(webDriver);
+            setColumnLocators(ImmutableMap.<String, String>builder()
+                    .put("sequence", "sequence")
+                    .put("id", "id")
+                    .put("orderId", "order-id")
+                    .put(COLUMN_TRACKING_ID, "tracking-id")
+                    .put(COLUMN_TYPE, "type")
+                    .put("shipper", "shipper")
+                    .put("address", "address")
+                    .put("routeId", "route-id")
+                    .put("status", "status")
+                    .put("startDateTime", "start-date-time")
+                    .put("endDateTime", "end-date-time")
+                    .put("dp", "dp")
+                    .put("pickupSize", "pickup-size")
+                    .put("comments", "comments")
+                    .build()
+            );
+            setEntityClass(TxnRsvn.class);
+            setMdVirtualRepeat(MD_VIRTUAL_REPEAT);
+        }
     }
 }
--- src/test/resources/cucumber/feature/create_route_groups.feature
+++ src/test/resources/cucumber/feature/create_route_groups.feature
@@ -1,4 +1,4 @@
-@OperatorV2 @OperatorV2Part2 @CreateRouteGroups @Saas
+@OperatorV2 @OperatorV2Part2 @CreateRouteGroups @Saas @ForceNotHeadless
 Feature: Transactions
 
   @LaunchBrowser @ShouldAlwaysRun
@@ -24,6 +24,71 @@ Feature: Transactions
     Given Operator go to menu Routing -> 2. Route Group Management
     When Operator wait until 'Route Group Management' page is loaded
     Then Operator V2 clean up 'Route Groups'
+
+  @DeleteOrArchiveRoute
+  Scenario: Operator Filter RTS Transaction (<hiptest-uid>)
+    Given API Shipper create V4 order using data below:
+      | generateFromAndTo | RANDOM                                                                                                                                                                                                                                                                                                                           |
+      | v4OrderRequest    | { "service_type":"Parcel", "service_level":"Standard", "parcel_job":{ "is_pickup_required":false, "pickup_date":"{{next-1-day-yyyy-MM-dd}}", "pickup_timeslot":{ "start_time":"12:00", "end_time":"15:00"}, "delivery_start_date":"{{next-1-day-yyyy-MM-dd}}", "delivery_timeslot":{ "start_time":"09:00", "end_time":"22:00"}}} |
+    Given API Operator Global Inbound parcel using data below:
+      | globalInboundRequest | { "hubId":{hub-id} } |
+    Given API Operator create new route using data below:
+      | createRouteRequest | { "zoneId":{zone-id}, "hubId":{hub-id}, "vehicleId":{vehicle-id}, "driverId":{ninja-driver-id} } |
+    Given API Operator add parcel to the route using data below:
+      | addParcelToRouteRequest | { "type":"DD" } |
+    Given API Operator RTS created order:
+      | rtsRequest | {"reason":"Return to sender: Nobody at address","timewindow_id":1,"date":"{gradle-next-1-day-yyyy-MM-dd}"} |
+    And API Operator get order details
+    Given Operator go to menu Routing -> 1. Create Route Groups
+    Given Operator wait until 'Create Route Group' page is loaded
+    Given Operator removes all General Filters except following: "Creation Time"
+    Given Operator add following filters on General Filters section on Create Route Group page:
+      | Creation Time | Today |
+    Given Operator choose "Include Transactions" on Transaction Filters section on Create Route Group page
+    Given Operator add following filters on Transactions Filters section on Create Route Group page:
+      | RTS | Show |
+    Given Operator choose "Hide Reservations" on Reservation Filters section on Create Route Group page
+    Given Operator click Load Selection on Create Route Group page
+    Then Operator verify PICKUP Transaction/Reservarion record on Create Route Group page using data below:
+      | id            | GET_FROM_CREATED_ORDER                  |
+      | orderId       | GET_FROM_CREATED_ORDER                  |
+      | trackingId    | GET_FROM_CREATED_ORDER                  |
+      | type          | PICKUP Transaction                      |
+      | shipper       | GET_FROM_CREATED_ORDER                  |
+      | address       | GET_FROM_CREATED_ORDER                  |
+      | status        | Arrived at Sorting Hub                  |
+      | startDateTime | {gradle-next-1-day-yyyy-MM-dd} 12:00:00 |
+      | endDateTime   | {gradle-next-1-day-yyyy-MM-dd} 15:00:00 |
+
+  @DeleteOrArchiveRoute
+  Scenario: Operator Filter Routed Transaction (<hiptest-uid>)
+    Given API Shipper create V4 order using data below:
+      | generateFromAndTo | RANDOM                                                                                                                                                                                                                                                                                                                          |
+      | v4OrderRequest    | { "service_type":"Return", "service_level":"Standard", "parcel_job":{ "is_pickup_required":true, "pickup_date":"{{next-1-day-yyyy-MM-dd}}", "pickup_timeslot":{ "start_time":"12:00", "end_time":"15:00"}, "delivery_start_date":"{{next-1-day-yyyy-MM-dd}}", "delivery_timeslot":{ "start_time":"09:00", "end_time":"22:00"}}} |
+    And API Operator create new route using data below:
+      | createRouteRequest | { "zoneId":{zone-id}, "hubId":{hub-id}, "vehicleId":{vehicle-id}, "driverId":{ninja-driver-id} } |
+    And API Operator add parcel to the route using data below:
+      | addParcelToRouteRequest | { "type":"PP" } |
+    And API Operator get order details
+    When Operator go to menu Routing -> 1. Create Route Groups
+    And Operator wait until 'Create Route Group' page is loaded
+    And Operator removes all General Filters except following: "Creation Time, Routed"
+    And Operator add following filters on General Filters section on Create Route Group page:
+      | Creation Time | Today |
+      | Routed        | Show  |
+    And Operator choose "Include Transactions" on Transaction Filters section on Create Route Group page
+    And Operator choose "Hide Reservations" on Reservation Filters section on Create Route Group page
+    And Operator click Load Selection on Create Route Group page
+    Then Operator verify PICKUP Transaction/Reservarion record on Create Route Group page using data below:
+      | id            | GET_FROM_CREATED_ORDER                  |
+      | orderId       | GET_FROM_CREATED_ORDER                  |
+      | trackingId    | GET_FROM_CREATED_ORDER                  |
+      | type          | PICKUP Transaction                      |
+      | shipper       | GET_FROM_CREATED_ORDER                  |
+      | address       | GET_FROM_CREATED_ORDER                  |
+      | status        | Pending Pickup                          |
+      | startDateTime | {gradle-next-1-day-yyyy-MM-dd} 12:00:00 |
+      | endDateTime   | {gradle-next-1-day-yyyy-MM-dd} 15:00:00 |
 
   @KillBrowser @ShouldAlwaysRun
   Scenario: Kill Browser
