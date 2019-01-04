c2e53ed2aba12632869e8869bb47d2792076d36b
Daniel Joi Partogi Hutapea <daniel.joi.ph@gmail.com>
2019-01-04 11:40:23 +0700

    Fix issue with gradle build.

---
+++ src/test/java/co/nvqa/operator_v2/TestRunner.java
@@ -0,0 +1,14 @@
+package co.nvqa.operator_v2;
+
+/**
+ * Dummy main class to test anything.
+ *
+ * @author Daniel Joi Partogi Hutapea
+ */
+public class TestRunner
+{
+    @SuppressWarnings("EmptyMethod")
+    public static void main(String[] args)
+    {
+    }
+}
