d106ae2b0bee04e69dcbb616182c52f81c37e504
Ian Gumilang <ian.gomu@gmail.com>
2020-05-13 10:14:07 +0700

    [UPDATE] remove source ~/.rvm/scripts/rvm

--- run-hiptest-publish-result.sh
+++ run-hiptest-publish-result.sh
@@ -2,12 +2,6 @@
 
 # reload RVM & hiptest-publisher
 
-rvmFile=~/.rvm/scripts/rvm
-
-if [[ -f "$rvmFile" ]]; then
-    source ${rvmFile}
-fi
-
 hiptest-publisher \
   --config-file hiptest-publisher.config \
   --push "build/reports/cucumber-junit/*.xml" \
