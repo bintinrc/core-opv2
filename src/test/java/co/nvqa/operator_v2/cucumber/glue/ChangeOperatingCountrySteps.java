63f4f890fab59441a301d7c7d3e24174c622d624
refoilmiya <refo.ilmiya@gmail.com>
2020-09-03 13:50:05 +0700

    This commit add a test on changing the operating country

---
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/ChangeOperatingCountrySteps.java
@@ -0,0 +1,20 @@
+package co.nvqa.operator_v2.cucumber.glue;
+
+import co.nvqa.operator_v2.selenium.page.ProfilePage;
+import cucumber.api.java.en.Then;
+
+public class ChangeOperatingCountrySteps extends AbstractSteps {
+    private ProfilePage profilePage;
+
+    @Override
+    public void init() {
+        profilePage = new ProfilePage(getWebDriver());
+    }
+
+    @Then("Operator verify operating country is {string}")
+    public void operatorVerifyOperatingCountryIsCorrect(String country) {
+        profilePage.currentCountryIs(country);
+    }
+
+
+}
