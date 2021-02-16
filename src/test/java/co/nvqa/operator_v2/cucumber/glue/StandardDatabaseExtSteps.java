be82ad5dbd45f122165142048c110a2f13dc334b
Refo Ilmiya <refo.akbar@ninjavan.co>
2021-02-16 11:33:18 +0700

    Fixed errors in path invalidation

--- src/test/java/co/nvqa/operator_v2/cucumber/glue/StandardDatabaseExtSteps.java
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/StandardDatabaseExtSteps.java
@@ -1317,6 +1317,12 @@ public class StandardDatabaseExtSteps extends AbstractDatabaseSteps<ScenarioMana
   public void dbOperatorVerifySlaInMovementEventsTableForNoPathForTheFollowingShipmentsFromTo(
       String scheduleType, String originHub, String destHub, List<String> shipmentIds) {
     switch (scheduleType) {
+      case HUB_CD_CD:
+        dbOperatorVerifySlaFailedAndPathNotFoundInExtDataMovementEventsTableWithDataBelow(
+            "FAILED", originHub, destHub, shipmentIds.subList(0, 0));
+        dbOperatorVerifySlaFailedAndPathNotFoundInExtDataMovementEventsTableWithDataBelow(
+            "NOT FOUND", originHub, destHub, shipmentIds.subList(1, 1));
+        break;
       case HUB_ST_ST_DIFF_CD:
       case HUB_ST_CD_DIFF_CD:
       case HUB_CD_ST_DIFF_CD:
@@ -1324,7 +1330,6 @@ public class StandardDatabaseExtSteps extends AbstractDatabaseSteps<ScenarioMana
         dbOperatorVerifySlaFailedAndPathNotFoundInExtDataMovementEventsTableWithDataBelow(
             "FAILED", originHub, destHub, shipmentIds);
         break;
-      case HUB_CD_CD:
       case HUB_CD_ITS_ST:
       case HUB_ST_ITS_CD:
         dbOperatorVerifySlaFailedAndPathNotFoundInExtDataMovementEventsTableWithDataBelow(
