b6d64a8b762136281009e0761c3c814f56aa4264
Nadeera Deraniyagala <nadeera.deraniyagala@ninjavan.co>
2021-04-12 12:57:39 +0800

    Adding more scenarios for "Search Invoiced Order" page

--- src/test/java/co/nvqa/operator_v2/selenium/page/InvoicedOrdersSearchPage.java
+++ src/test/java/co/nvqa/operator_v2/selenium/page/InvoicedOrdersSearchPage.java
@@ -1,13 +1,16 @@
 package co.nvqa.operator_v2.selenium.page;
 
+import co.nvqa.commons.util.NvLogger;
+import co.nvqa.operator_v2.selenium.elements.Button;
 import co.nvqa.operator_v2.selenium.elements.PageElement;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
 public class InvoicedOrdersSearchPage extends OperatorV2SimplePage {
 
 
-  @FindBy(xpath = "//label[@class='ant-radio-button-wrapper ant-radio-button-wrapper-checked']//span[text()='Upload CSV']")
+  @FindBy(xpath = "//span[@class='ant-radio-button']/following-sibling::span/span[text()='Upload CSV']")
   public PageElement uploadCsvButton;
   @FindBy(xpath = "//span[text()='Enter Tracking ID(s)']")
   public PageElement trackingIDsButton;
@@ -19,8 +22,14 @@ public class InvoicedOrdersSearchPage extends OperatorV2SimplePage {
   public PageElement trackingIdInput;
   @FindBy(xpath = "//td[@class='trackingId']/span/span")
   public PageElement trackingIdOutput;
+  @FindBy(xpath = "//span[text()='Tracking Number']//following::input")
+  public PageElement trackingIdSearchInput;
+  @FindBy(xpath = "//mark[@class='highlight ']")
+  public PageElement searchOutput;
   @FindBy(xpath = "//td[@class='createdAtStr']/span/span")
   public PageElement createdAtOutput;
+  @FindBy(xpath = "//span[text()='Creation Time']//following::input")
+  public PageElement createdAtSearchInput;
   @FindBy(xpath = "//strong[text()='# Tracking IDs from CSV']/parent::span/following-sibling::span")
   public PageElement orderCount;
   @FindBy(xpath = "//span[text()='No Results Found']")
@@ -29,16 +38,25 @@ public class InvoicedOrdersSearchPage extends OperatorV2SimplePage {
   public PageElement antNotificationMessage;
   @FindBy(xpath = "//div[@class='ant-notification-notice-description']")
   public PageElement antNotificationMessageDescription;
+  @FindBy(xpath = "//span[text()='Search Invoiced Orders']/parent::button/preceding::p[1]")
+  public PageElement errorMessageText;
   @FindBy(tagName = "iframe")
   private PageElement pageFrame;
-
+  @FindBy(id = "refresh-btn")
+  public Button refreshBtn;
+  @FindBy(id = "go-back-btn")
+  public Button goBackToFilterBtn;
 
   public InvoicedOrdersSearchPage(WebDriver webDriver) {
     super(webDriver);
   }
 
   public void switchTo() {
-    getWebDriver().switchTo().frame(pageFrame.getWebElement());
+    try {
+      getWebDriver().switchTo().frame(pageFrame.getWebElement());
+    } catch (NoSuchElementException e) {
+      NvLogger.info("Already inside the iframe");
+    }
   }
 
   public void uploadFile(String path) {
@@ -50,10 +68,6 @@ public class InvoicedOrdersSearchPage extends OperatorV2SimplePage {
   public String getNotificationMessageText() {
     antNotificationMessage.waitUntilVisible();
     return antNotificationMessage.getText();
-//    assertThat("Notification message is the same", actualNotificationMessage,
-//        equalTo(expectedNotificationMessage));
-//    closeAntNotificationMessage.click();
-//    antNotificationMessage.waitUntilInvisible();
   }
 
   public String getNotificationMessageDescText() {
