031337e2053050316c3cd01dac792874000a1984
Refo Ilmiya <refo.akbar@ninjavan.co>
2021-04-23 12:49:38 +0700

    Fixed shipment management

--- src/test/java/co/nvqa/operator_v2/cucumber/glue/ShipmentManagementSteps.java
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/ShipmentManagementSteps.java
@@ -539,7 +539,7 @@ public class ShipmentManagementSteps extends AbstractSteps {
 
   @And("^Operator select created filters preset on Shipment Management page$")
   public void operatorSelectCreatedFiltersPresetOnShipmentManagementPage() {
-    String presetName = get(KEY_SHIPMENT_MANAGEMENT_FILTERS_PRESET_ID) + "-" + get(
+    String presetName = get(KEY_SHIPMENT_MANAGEMENT_FILTERS_PRESET_ID) + " - " + get(
         KEY_SHIPMENT_MANAGEMENT_FILTERS_PRESET_NAME);
     operatorSelectGivenFiltersPresetOnShipmentManagementPage(presetName);
   }
