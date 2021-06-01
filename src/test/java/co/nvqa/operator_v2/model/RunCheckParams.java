071827a8e141b63d1ab1bae72a140c01b43e06da
Weam Khalaf <weam.khalaf@outlook.com>
2021-06-01 07:06:23 +0300

    remove lombok

--- src/test/java/co/nvqa/operator_v2/model/RunCheckParams.java
+++ src/test/java/co/nvqa/operator_v2/model/RunCheckParams.java
@@ -3,14 +3,6 @@ package co.nvqa.operator_v2.model;
 /**
  * @author Daniel Joi Partogi Hutapea
  */
-
-import lombok.Setter;
-import lombok.Getter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@ToString
 public class RunCheckParams {
 
   private String orderFields;
@@ -35,4 +27,180 @@ public class RunCheckParams {
   private String isL1Exist;
   private String isL2Exist;
   private String isL3Exist;
+
+  public String getOrderFields() {
+    return orderFields;
+  }
+
+  public void setOrderFields(String orderFields) {
+    this.orderFields = orderFields;
+  }
+
+  public String getDeliveryType() {
+    return deliveryType;
+  }
+
+  public void setDeliveryType(String deliveryType) {
+    this.deliveryType = deliveryType;
+  }
+
+  public String getOrderType() {
+    return orderType;
+  }
+
+  public void setOrderType(String orderType) {
+    this.orderType = orderType;
+  }
+
+  public String getServiceLevel() {
+    return serviceLevel;
+  }
+
+  public void setServiceLevel(String serviceLevel) {
+    this.serviceLevel = serviceLevel;
+  }
+
+  public String getServiceType() {
+    return serviceType;
+  }
+
+  public void setServiceType(String serviceType) {
+    this.serviceType = serviceType;
+  }
+
+  public String getTimeslotType() {
+    return timeslotType;
+  }
+
+  public void setTimeslotType(String timeslotType) {
+    this.timeslotType = timeslotType;
+  }
+
+  public String getIsRts() {
+    return isRts;
+  }
+
+  public void setIsRts(String isRts) {
+    this.isRts = isRts;
+  }
+
+  public String getSize() {
+    return size;
+  }
+
+  public void setSize(String size) {
+    this.size = size;
+  }
+
+  public Double getWeight() {
+    return weight;
+  }
+
+  public void setWeight(Double weight) {
+    this.weight = weight;
+  }
+
+  public Double getInsuredValue() {
+    return insuredValue;
+  }
+
+  public void setInsuredValue(Double insuredValue) {
+    this.insuredValue = insuredValue;
+  }
+
+  public Double getCodValue() {
+    return codValue;
+  }
+
+  public void setCodValue(Double codValue) {
+    this.codValue = codValue;
+  }
+
+  public String getFromZone() {
+    return fromZone;
+  }
+
+  public void setFromZone(String fromZone) {
+    this.fromZone = fromZone;
+  }
+
+  public String getToZone() {
+    return toZone;
+  }
+
+  public void setToZone(String toZone) {
+    this.toZone = toZone;
+  }
+
+  public String getFromL1() {
+    return fromL1;
+  }
+
+  public void setFromL1(String fromL1) {
+    this.fromL1 = fromL1;
+  }
+
+  public String getFromL2() {
+    return fromL2;
+  }
+
+  public void setFromL2(String fromL2) {
+    this.fromL2 = fromL2;
+  }
+
+  public String getFromL3() {
+    return fromL3;
+  }
+
+  public void setFromL3(String fromL3) {
+    this.fromL3 = fromL3;
+  }
+
+  public String getToL1() {
+    return toL1;
+  }
+
+  public void setToL1(String toL1) {
+    this.toL1 = toL1;
+  }
+
+  public String getToL2() {
+    return toL2;
+  }
+
+  public void setToL2(String toL2) {
+    this.toL2 = toL2;
+  }
+
+  public String getToL3() {
+    return toL3;
+  }
+
+  public void setToL3(String toL3) {
+    this.toL3 = toL3;
+  }
+
+  public String getIsL1Exist() {
+    return isL1Exist;
+  }
+
+  public void setIsL1Exist(String isL1Exist) {
+    this.isL1Exist = isL1Exist;
+  }
+
+  public String getIsL2Exist() {
+    return isL2Exist;
+  }
+
+  public void setIsL2Exist(String isL2Exist) {
+    this.isL2Exist = isL2Exist;
+  }
+
+  public String getIsL3Exist() {
+    return isL3Exist;
+  }
+
+  public void setIsL3Exist(String isL3Exist) {
+    this.isL3Exist = isL3Exist;
+  }
 }
