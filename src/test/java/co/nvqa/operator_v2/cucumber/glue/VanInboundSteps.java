54f8edcda5019c31bec0bb0220bad6e3aa482ee6
felix <felix.soewito@gmail.com>
2021-10-31 23:11:19 +0700

    assertions and logger refactor

--- src/test/java/co/nvqa/operator_v2/cucumber/glue/VanInboundSteps.java
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/VanInboundSteps.java
@@ -24,30 +24,30 @@ public class VanInboundSteps extends AbstractSteps {
     editOrderPage = new EditOrderPage(getWebDriver());
   }
 
-  @And("^Operator fill the route ID on Van Inbound Page then click enter$")
+  @And("Operator fill the route ID on Van Inbound Page then click enter")
   public void fillRouteIdOnVanInboundPage() {
     Long routeId = get(KEY_CREATED_ROUTE_ID);
     vanInboundPage.fillRouteIdOnVanInboundPage(String.valueOf(routeId));
   }
 
-  @And("^Operator fill the tracking ID on Van Inbound Page then click enter$")
+  @And("Operator fill the tracking ID on Van Inbound Page then click enter")
   public void fillTrackingIdOnVanInboundPage() {
     String trackingId = get(KEY_CREATED_ORDER_TRACKING_ID);
     vanInboundPage.fillTrackingIdOnVanInboundPage(trackingId);
   }
 
-  @Then("^Operator verify the van inbound process is succeed$")
+  @Then("Operator verify the van inbound process is succeed")
   public void verifyVanInboundSucceed() {
     vanInboundPage.verifyVanInboundSucceed();
   }
 
-  @Then("^Operator verify order scan updated$")
+  @Then("Operator verify order scan updated")
   public void verifyOrderScanUpdated() {
     editOrderPage.waitUntilInvisibilityOfLoadingOrder();
     editOrderPage.verifyInboundIsSucceed();
   }
 
-  @And("^Operator click on start route after van inbounding$")
+  @And("Operator click on start route after van inbounding")
   public void startRoute() {
     String trackingId = get(KEY_CREATED_ORDER_TRACKING_ID);
     vanInboundPage.startRoute(trackingId);
@@ -63,13 +63,13 @@ public class VanInboundSteps extends AbstractSteps {
     vanInboundPage.verifyInvalidTrackingId(trackingId);
   }
 
-  @And("^Operator fill the empty tracking ID on Van Inbound Page$")
+  @And("Operator fill the empty tracking ID on Van Inbound Page")
   public void emptyTrackingId() {
     String trackingId = "";
     vanInboundPage.fillTrackingIdOnVanInboundPage(trackingId);
   }
 
-  @Then("^Operator verify the tracking ID that has been input on Van Inbound Page is empty$")
+  @Then("Operator verify the tracking ID that has been input on Van Inbound Page is empty")
   public void verifyTrackingIdEmpty() {
     vanInboundPage.verifyTrackingIdEmpty();
   }
