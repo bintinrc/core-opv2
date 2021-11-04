9a46c80edda2dd7b4548a05298ec69fc08644873
Sergei Mishanin <sergei.mishanin1985@gmail.com>
2021-11-05 02:27:40 +0800

    new Core scenarios

--- src/test/java/co/nvqa/operator_v2/cucumber/glue/VanInboundSteps.java
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/VanInboundSteps.java
@@ -2,9 +2,16 @@ package co.nvqa.operator_v2.cucumber.glue;
 
 import co.nvqa.operator_v2.selenium.page.EditOrderPage;
 import co.nvqa.operator_v2.selenium.page.VanInboundPage;
+import co.nvqa.operator_v2.selenium.page.VanInboundPage.ScannedParcelsDialog.OrderInfo;
+import io.cucumber.guice.ScenarioScoped;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
-import io.cucumber.guice.ScenarioScoped;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
+import org.assertj.core.api.Assertions;
+import org.openqa.selenium.Keys;
 
 /**
  * @author Tristania Siagian
@@ -36,9 +43,50 @@ public class VanInboundSteps extends AbstractSteps {
     vanInboundPage.fillTrackingIdOnVanInboundPage(trackingId);
   }
 
+  @And("Operator enters {value} tracking ID and press enter on Van Inbound Page")
+  public void fillTrackingIdOnVanInboundPage(String trackingId) {
+    vanInboundPage.trackingId.setValue(trackingId + Keys.ENTER);
+  }
+
+  @And("Operator scan {value} tracking ID on Van Inbound Page")
+  public void scanTrackingIdOnVanInboundPage(String trackingId) {
+    vanInboundPage.trackingIdScan.setValue(trackingId + Keys.ENTER);
+    pause2s();
+  }
+
   @Then("Operator verify the van inbound process is succeed")
   public void verifyVanInboundSucceed() {
     vanInboundPage.verifyVanInboundSucceed();
+  }
+
+  @Then("Operator verifies {string} scanned parcels displayed on Van Inbound Page")
+  public void verifyScannedParcels(String expected) {
+    Assertions.assertThat(vanInboundPage.scannedParcelsCount.getText())
+        .as("Scanned parcels label")
+        .isEqualTo(expected.trim() + " Parcels");
+  }
+
+  @Then("Operator click Scanned Parcels area on Van Inbound Page")
+  public void clickScannedParcels() {
+    vanInboundPage.scannedParcelsCount.click();
+  }
+
+  @Then("Operator verifies Scanned Parcels dialog contains orders info:")
+  public void checkScannedParcelsDialogTable(List<Map<String, String>> data) {
+    data = resolveListOfMaps(data);
+    vanInboundPage.scannedParcelsDialog.waitUntilVisible();
+    pause2s();
+    List<OrderInfo> actual = vanInboundPage.scannedParcelsDialog.ordersTable.readAllEntities();
+    List<OrderInfo> expected = data.stream().map(OrderInfo::new).collect(Collectors.toList());
+
+    expected.forEach(item -> {
+      OrderInfo actualItem = actual.stream()
+          .filter(o -> StringUtils.equals(o.getTrackingId(), item.getTrackingId()))
+          .findFirst()
+          .orElseThrow(() -> new AssertionError(
+              "Order with Tracking Id " + item.getTrackingId() + " was not found"));
+      item.compareWithActual(actualItem);
+    });
   }
 
   @Then("Operator verify order scan updated")
