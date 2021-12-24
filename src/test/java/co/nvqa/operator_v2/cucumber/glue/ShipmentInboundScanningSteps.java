a5988c257e2376a2780303d02355a3837e3ba33c
Meganathan Ramasamy <ec-meganathan.ramasamy@ninjavan.co>
2021-12-24 03:14:52 +0000

    added format date fix for Shiment inbound scanning

--- src/test/java/co/nvqa/operator_v2/cucumber/glue/ShipmentInboundScanningSteps.java
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/ShipmentInboundScanningSteps.java
@@ -37,7 +37,6 @@ public class ShipmentInboundScanningSteps extends AbstractSteps {
     retryIfRuntimeExceptionOccurred(() ->
     {
       try {
-        //System.out.println(""+getScenarioStorage().get(KEY_CREATED_SHIPMENT_ID));
         Long shipmentId = (Long)getScenarioStorage().get(KEY_CREATED_SHIPMENT_ID);
         final String finalHub = resolveValue(hub);
         scanningPage.inboundScanning(shipmentId, label, finalHub);
