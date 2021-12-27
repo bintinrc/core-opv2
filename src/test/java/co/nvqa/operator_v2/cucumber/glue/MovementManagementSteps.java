aec86bb1718336044cd834c4599a729b7ba60a6f
Meganathan Ramasamy <ec-meganathan.ramasamy@ninjavan.co>
2021-12-27 15:01:11 +0000

    review comment fix

--- src/test/java/co/nvqa/operator_v2/cucumber/glue/MovementManagementSteps.java
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/MovementManagementSteps.java
@@ -95,7 +95,7 @@ public class MovementManagementSteps extends AbstractSteps {
 
   @When("Movement Management page is loaded")
   public void movementManagementPageIsLoaded() {
-    pause5s();
+    movementManagementPage.addSchedule.waitUntilVisible(10);
     movementManagementPage.switchTo();
     movementManagementPage.addSchedule.waitUntilClickable(60);
   }
