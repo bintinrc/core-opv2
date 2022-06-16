509362893d79cb4eac67e92ffce84774367fa291
Veera N <nmani0303@hotmail.com>
2022-06-16 23:25:59 +0530

    Updating XPath without modifing the existing

--- src/test/java/co/nvqa/operator_v2/selenium/page/AntTableV3.java
+++ src/test/java/co/nvqa/operator_v2/selenium/page/AntTableV3.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.support.PageFactory;
  */
 public class AntTableV3<T extends DataEntity<?>> extends AbstractTable<T> {
 
-  private static final String CELL_LOCATOR_PATTERN = "//tr[contains(@class,'ant-table-row')][%d]/td[%s]";
+  private static final String CELL_LOCATOR_PATTERN = "//tr[contains(@class,'ant-table-row')][%d]/td[contains(@class, '%s')] | //tr[contains(@class,'ant-table-row')][%1$d]/td[%2$s]";
   private static final String ACTION_BUTTON_LOCATOR_PATTERN = "(//tr[contains(@class,'ant-table-row')][%d]//td[contains(@class,'ant-table-cell-fix-right')]//button)[%s]";
 
   @FindBy(className = "ant-empty-description")
