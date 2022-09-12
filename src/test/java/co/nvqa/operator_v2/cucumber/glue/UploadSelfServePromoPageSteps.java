62c6b21cb29d4e425a6f4a64dbdd6ee0a1bab3d0
Nadeera Deraniyagala <nadeera.deraniyagala@ninjavan.co>
2022-09-12 18:43:40 +0800

    add new scenarios for self serve page

--- src/test/java/co/nvqa/operator_v2/cucumber/glue/UploadSelfServePromoPageSteps.java
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/UploadSelfServePromoPageSteps.java
@@ -6,12 +6,17 @@ import co.nvqa.operator_v2.selenium.page.UploadSelfServePromoPage;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import java.io.File;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,7 +25,8 @@ import static co.nvqa.commons.util.StandardTestUtils.createFile;
 
 public class UploadSelfServePromoPageSteps extends AbstractSteps {
 
-  public static final String CSV_FILENAME_PATTERN = "upload_pricing_profiles.csv";
+  public static final String CSV_FILENAME_PATTERN = "automation_upload_pricing_profiles.csv";
+  public static final String PRICING_PROFILE_ERRORS_CSV_FILENAME_PATTERN = "upload_pricing_profile_errors";
   private static final Logger LOGGER = LoggerFactory.getLogger(UploadSelfServePromoPageSteps.class);
   private UploadSelfServePromoPage uploadSelfServePromoPage;
 
@@ -44,16 +50,34 @@ public class UploadSelfServePromoPageSteps extends AbstractSteps {
 
   @And("Operator uploads csv file with below data:")
   public void operatorUploadsCsvFileWithBelowData(DataTable dt) {
-
     List<Pricing> pricingProfiles = resolveDataTableToList(dt, Pricing.class);
     put(KEY_PRICING_PROFILE, pricingProfiles.get(0));
+    put(KEY_LIST_OF_PRICING_PROFILES, pricingProfiles);
 
+    File csvFile = getCsvFile(dt);
+    uploadSelfServePromoPage.inFrame(() ->
+        uploadSelfServePromoPage.uploadBulkPricingProfileDialog.uploadFile(csvFile)
+    );
+  }
+
+  @NotNull
+  private File getCsvFile(DataTable dt) {
     List<List<String>> rows = resolveListOfLists(dt.asLists());
-
     String sb = rows.stream().map(row -> String.join(",", row))
         .collect(Collectors.joining("\n"));
     File csvFile = createFile(CSV_FILENAME_PATTERN, sb);
     LOGGER.info("Path of the created file " + csvFile.getAbsolutePath());
+    return csvFile;
+  }
+
+
+  @And("Operator successfully uploads csv file with below data:")
+  public void operatorSuccessUploadsCsvFileWithBelowData(DataTable dt) {
+    List<Pricing> pricingProfiles = resolveDataTableToList(dt, Pricing.class);
+    put(KEY_PRICING_PROFILE, pricingProfiles.get(0));
+    put(KEY_LIST_OF_PRICING_PROFILES, pricingProfiles);
+
+    File csvFile = getCsvFile(dt);
 
     uploadSelfServePromoPage.inFrame(() -> {
       uploadSelfServePromoPage.uploadBulkPricingProfileDialog.uploadFile(csvFile);
@@ -90,6 +114,73 @@ public class UploadSelfServePromoPageSteps extends AbstractSteps {
         .isEqualTo(pricingProfileFromDb.getEffectiveDate().toString().split(" ")[0]);
 
     softAssertions.assertAll();
+  }
+
+  @Given("Operator clicks Download Sample CSV Template button on the Upload Self Serve Promo Page")
+  public void operatorClicksDownloadSampleCSVTemplateButtonOnTheUploadSelfServePromoPage() {
+    uploadSelfServePromoPage.inFrame(() ->
+        uploadSelfServePromoPage.downloadSampleCsvButton());
+  }
+
+
+  @Then("Operator verify Sample CSV file on Upload Self Serve Promo Page is downloaded successfully with below data")
+  public void operatorVerifySampleCSVFileOnUploadSelfServePromoPageIsDownloadedSuccessfullyWithBelowData(
+      String expectedString) {
+    uploadSelfServePromoPage.verifyCsvFileDownloadedSuccessfully(expectedString);
+  }
+
+  @Then("Operator verifies error message {string} in Upload Self Serve Promo Page")
+  public void operatorVerifiesErrorMessageInUploadSelfServePromoPage(String expectedErrorMsg) {
+    uploadSelfServePromoPage.inFrame(() -> {
+      String validateNotificationText = uploadSelfServePromoPage.getNotificationMessageText();
+      Assertions.assertThat(validateNotificationText).as("Expected Error Msg is visible")
+          .isEqualTo(expectedErrorMsg);
+    });
+  }
+
+  @Then("Operator clicks submit button on the Upload Self Serve Promo Page")
+  public void operatorClicksSubmitButtonOnTheUploadSelfServePromoPage() {
+    uploadSelfServePromoPage.inFrame(() ->
+        uploadSelfServePromoPage.uploadBulkPricingProfileDialog.submitButton.click()
+    );
+  }
+
+  @Then("Operator verifies that error toast is displayed on Upload Self Serve Promo Page:")
+  public void operatorVerifiesThatErrorToastDisplayedOnUploadSelfServePromoPage(
+      Map<String, String> mapOfData) {
+    mapOfData = resolveKeyValues(mapOfData);
+    String errorTitle = mapOfData.get("top");
+    String errorMessage = mapOfData.get("bottom");
+    boolean isErrorFound = false;
+    if (Objects.nonNull(errorTitle) && Objects.nonNull(errorMessage)) {
+      isErrorFound = uploadSelfServePromoPage.toastErrors.stream().anyMatch(toastError ->
+          StringUtils.equalsIgnoreCase(toastError.toastTop.getText(), errorTitle)
+              && StringUtils
+              .containsIgnoreCase(toastError.toastBottom.getText(), errorMessage));
+    } else if (Objects.nonNull(errorTitle)) {
+      isErrorFound = uploadSelfServePromoPage.toastErrors.stream().anyMatch(toastError ->
+          StringUtils.equalsIgnoreCase(toastError.toastTop.getText(), errorTitle));
+    }
+
+    Assertions.assertThat(isErrorFound).as("Error message is exist").isTrue();
+  }
+
+  @Then("Operator clicks Download Errors CSV on Upload Self Serve Promo Page")
+  public void operatorClicksDownloadErrorsCSVOnUploadSelfServePromoPage() {
+    uploadSelfServePromoPage.inFrame(() ->
+        uploadSelfServePromoPage.uploadBulkPricingProfileDialog.clickDownloadErrorsCsvButton()
+    );
+  }
+
+  @Then("Operator verify Download Errors CSV file on Upload Self Serve Promo Page is downloaded successfully with below data:")
+  public void operatorVerifyDownloadErrorsCSVFileOnUploadSelfServePromoPageIsDownloadedSuccessfullyWithBelowData(
+      DataTable dt) {
+    List<List<String>> rows = resolveListOfLists(dt.asLists());
+    String sb = rows.stream().map(row -> String.join(",", row))
+        .collect(Collectors.joining("\n"));
+
+    uploadSelfServePromoPage.verifyDownloadErrorsCsvFileDownloadedSuccessfully(sb,
+        PRICING_PROFILE_ERRORS_CSV_FILENAME_PATTERN);
   }
 
 }
