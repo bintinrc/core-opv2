22ebc841b3c00c787e38658f27d7cc0190de38a8
Son Ha <ec-son.ha@ninjavan.co>
2023-01-19 16:30:32 +0700

    NVQA-8204

--- src/test/java/co/nvqa/operator_v2/selenium/page/pickupAppointment/PickupAppointmentJobPageV2.java
+++ src/test/java/co/nvqa/operator_v2/selenium/page/pickupAppointment/PickupAppointmentJobPageV2.java
@@ -588,6 +588,9 @@ public class PickupAppointmentJobPageV2 extends SimpleReactPage<PickupAppointmen
     @FindBy(xpath = "//div[text()='Jobs created']")
     public PageElement title;
 
+    @FindBy(xpath = "//div[text()='Job created']")
+    public PageElement title2;
+
     @FindBy(xpath = "//span[text()='Time slot:']/following::span")
     public PageElement createdTime;
 
@@ -595,6 +598,16 @@ public class PickupAppointmentJobPageV2 extends SimpleReactPage<PickupAppointmen
     public Button confirm;
 
     public final String COLUMN_DATA_XPATH = "//tbody[@class='ant-table-tbody']//td[text()='%s']";
+
+    @FindBy(xpath = "//span[text()='Ready by:']/following-sibling::span")
+    public PageElement startTime;
+
+    @FindBy(xpath = "//span[text()='Latest by:']/following-sibling::span")
+    public PageElement endTime;
+
+    @FindBy(xpath = "//span[text()='Jobs created for the following dates:']/following-sibling::span")
+    public PageElement followingDates;
+
 
   }
 
@@ -628,6 +641,9 @@ public class PickupAppointmentJobPageV2 extends SimpleReactPage<PickupAppointmen
 
     String JOB_TAG_REMOVE_XPATH = "//span[@class='ant-select-selection-item-content' and text()='%s']/following-sibling::span[contains(@class,'item-remove')]";
 
+    @FindBy(xpath = "//button[. = 'Create new job']")
+    public Button createNewJob;
+
   }
 
   public void setRouteOnEditPAJobPage(String routeId) {
@@ -658,8 +674,8 @@ public class PickupAppointmentJobPageV2 extends SimpleReactPage<PickupAppointmen
     loadingIcon.waitUntilInvisible();
   }
 
-  public void removeTagOnEditJobpage(String tagName){
-    findElementByXpath(f(editPAJob.JOB_TAG_REMOVE_XPATH,tagName)).click();
+  public void removeTagOnEditJobpage(String tagName) {
+    findElementByXpath(f(editPAJob.JOB_TAG_REMOVE_XPATH, tagName)).click();
     editPAJob.updateTags.waitUntilClickable();
   }
 }
