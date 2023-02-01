17ae42c722b04464d6eabfb658bd852856e172cd
Indah Puspita <indah.p@ninjavan.co>
2023-02-01 17:35:19 +0700

    Add new feature for Update Billing Number SWB

--- src/test/java/co/nvqa/operator_v2/selenium/page/mm/shipmentweight/ShipmentWeightDimensionUpdateMawbPage.java
+++ src/test/java/co/nvqa/operator_v2/selenium/page/mm/shipmentweight/ShipmentWeightDimensionUpdateMawbPage.java
@@ -6,6 +6,7 @@ import co.nvqa.operator_v2.selenium.elements.ant.NvTable;
 import co.nvqa.operator_v2.selenium.elements.ant.v4.AntSelect;
 import co.nvqa.operator_v2.selenium.elements.mm.AntConfirmModal;
 import co.nvqa.operator_v2.selenium.page.SimpleReactPage;
+import org.assertj.core.api.Assertions;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -38,6 +39,48 @@ public class ShipmentWeightDimensionUpdateMawbPage  extends SimpleReactPage<Ship
   @FindBy(xpath = "//div[contains(@class, ' ant-select')][.//input[@id='UpdateBillNumberForm_destination_airport_id']]")
   public AntSelect destAirportSelect;
 
+  @FindBy(css = "[data-testid='seaway-bill-number']")
+  public PageElement swbInput;
+
+  @FindBy(xpath = "//div[contains(@class, ' ant-select')][.//input[@id='UpdateBillNumberForm_seahaul_vendor_id']]")
+  public AntSelect seahaulVendorSelect;
+
+  @FindBy(xpath = "//div[contains(@class, ' ant-select')][.//input[@id='UpdateBillNumberForm_origin_seaport_id']]")
+  public AntSelect originSeaportSelect;
+
+  @FindBy(xpath = "//div[contains(@class, ' ant-select')][.//input[@id='UpdateBillNumberForm_destination_seaport_id']]")
+  public AntSelect destinationSeaportSelect;
+
+  @FindBy(xpath = "//div[.//input[@data-testid='seaway-bill-number'] and contains(concat(' ',normalize-space(@class),' '),' ant-form-item-control ')]//div[@class='ant-form-item-explain-error']")
+  public PageElement swbErrorInfo;
+
+  @FindBy(xpath = "//div[.//input[@id='UpdateBillNumberForm_seahaul_vendor_id'] and contains(concat(' ',normalize-space(@class),' '),' ant-form-item-control ')]//div[@class='ant-form-item-explain-error']")
+  public PageElement emptySeaportVendorErrorInfo;
+
+  @FindBy(xpath = "//div[.//input[@id='UpdateBillNumberForm_origin_seaport_id'] and contains(concat(' ',normalize-space(@class),' '),' ant-form-item-control ')]//div[@class='ant-form-item-explain-error']")
+  public PageElement emptyOriginSeaportErrorInfo;
+
+  @FindBy(xpath = "//div[.//input[@id='UpdateBillNumberForm_destination_seaport_id'] and contains(concat(' ',normalize-space(@class),' '),' ant-form-item-control ')]//div[@class='ant-form-item-explain-error']")
+  public PageElement emptyDestinationSeaportErrorInfo;
+
+  @FindBy(xpath = "//div[@data-testid='seahaul-vendor-select']//span[@class='ant-select-clear']")
+  public Button clearSeahaulVendorButton;
+
+  @FindBy(xpath = "//div[@data-testid='origin-seaport-select']//span[@class='ant-select-clear']")
+  public Button clearOriginSeaportButton;
+
+  @FindBy(xpath = "//div[@data-testid='destination-seaport-select']//span[@class='ant-select-clear']")
+  public Button clearDestinationSeaportButton;
+
+  @FindBy(xpath = "//div[@data-testid='airhaul-vendor-select']//span[@class='ant-select-clear']")
+  public Button clearAirhaulVendorButton;
+
+  @FindBy(xpath = "//div[@data-testid='origin-airport-select']//span[@class='ant-select-clear']")
+  public Button clearOriginAirportButton;
+
+  @FindBy(xpath = "//div[@data-testid='destination-airport-select']//span[@class='ant-select-clear']")
+  public Button clearDestinationAirportButton;
+
   @FindBy(css = "[data-testid='bill-number-update-button']")
   public Button updateMawbBtn;
 
@@ -47,6 +90,35 @@ public class ShipmentWeightDimensionUpdateMawbPage  extends SimpleReactPage<Ship
   @FindBy(css = ".ant-modal-confirm")
   public AntConfirmModal confirmSameMawbDialog;
 
+  private static final String SEAHAUL_VENDOR_FIELD = "//div[contains(@class, ' ant-select')][.//input[@id='UpdateBillNumberForm_seahaul_vendor_id']]";
+  private static final String ORIGIN_SEAPORT_FIELD = "//div[contains(@class, ' ant-select')][.//input[@id='UpdateBillNumberForm_origin_seaport_id']]";
+  private static final String DESTINATION_SEAPORT_FIELD = "//div[contains(@class, ' ant-select')][.//input[@id='UpdateBillNumberForm_destination_seaport_id']]";
+  private static final String AIRHAUL_VENDOR_FIELD = "//div[contains(@class, ' ant-select')][.//input[@id='UpdateBillNumberForm_airhaul_vendor_id']]";
+  private static final String ORIGIN_AIRPORT_FIELD = "//div[contains(@class, ' ant-select')][.//input[@id='UpdateBillNumberForm_origin_airport_id']]";
+  private static final String DESTINATION_AIRPORT_FIELD = "//div[contains(@class, ' ant-select')][.//input[@id='UpdateBillNumberForm_destination_airport_id']]";
+
+  public void hoverUpdateBillNumberField(String billNumberField) {
+    switch (billNumberField) {
+      case "Sea Haul Vendor":
+        moveToElementWithXpath(SEAHAUL_VENDOR_FIELD);
+        break;
+      case "Origin Seaport":
+        moveToElementWithXpath(ORIGIN_SEAPORT_FIELD);
+        break;
+      case "Destination Seaport":
+        moveToElementWithXpath(DESTINATION_SEAPORT_FIELD);
+        break;
+      case "Air Haul Vendor":
+        moveToElementWithXpath(AIRHAUL_VENDOR_FIELD);
+        break;
+      case "Origin Airport":
+        moveToElementWithXpath(ORIGIN_AIRPORT_FIELD);
+        break;
+      case "Destination Airport":
+        moveToElementWithXpath(DESTINATION_AIRPORT_FIELD);
+        break;
+    }
+  }
 
   public static class ShipmentWeightUpdateMawbTableRow extends NvTable.NvRow {
 
@@ -59,7 +131,6 @@ public class ShipmentWeightDimensionUpdateMawbPage  extends SimpleReactPage<Ship
     @FindBy(xpath = "./td[contains(@class,'ant-table-cell weight')]")
     public PageElement weight;
 
-    //not capturing all columns
 
 
     public ShipmentWeightUpdateMawbTableRow(WebDriver webDriver,
