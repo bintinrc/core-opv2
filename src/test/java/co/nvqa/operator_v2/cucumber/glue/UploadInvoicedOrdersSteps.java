c7809ec6c6c99b4f5bbbe4ec0927b133300056b6
Nadeera Deraniyagala <nadeera.deraniyagala@ninjavan.co>
2023-02-13 15:28:21 -0800

    update classes/methods regarding upload invoiced orders page

--- src/test/java/co/nvqa/operator_v2/cucumber/glue/UploadInvoicedOrdersSteps.java
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/UploadInvoicedOrdersSteps.java
@@ -5,6 +5,7 @@ import co.nvqa.commons.util.NvLogger;
 import co.nvqa.operator_v2.selenium.page.UploadInvoicedOrdersPage;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import java.io.File;
 import java.util.List;
 import org.assertj.core.api.Assertions;
@@ -21,20 +22,13 @@ public class UploadInvoicedOrdersSteps extends AbstractSteps {
     uploadInvoicedOrdersPage = new UploadInvoicedOrdersPage(getWebDriver());
   }
 
-  @And("Operator clicks Upload Invoiced Orders with CSV button on the Upload Invoiced Orders Page")
-  public void operatorClicksUploadInvoicedOrdersWithCSVButtonOnTheUploadInvoicedOrdersPage() {
-    uploadInvoicedOrdersPage.clickUploadCsvButton();
-    uploadInvoicedOrdersPage.verifyUploadInvoicedOrdersDialogIsDisplayed();
-  }
-
   @And("Operator upload a CSV file with below order ids and verify success message")
   public void operatorUploadACSVFileWithBelowOrderIdsVerify(List<String> trackingIds) {
     trackingIds = resolveValues(trackingIds);
     File csvFile = StandardTestUtils.createFile("upload.csv", String.join("\n", trackingIds));
     NvLogger.info("Path of the created file : " + csvFile.getAbsolutePath());
-    uploadInvoicedOrdersPage.uploadInvoicedOrdersDialog.uploadFile(csvFile);
+    uploadInvoicedOrdersPage.uploadFile(csvFile);
     uploadInvoicedOrdersPage.verifySuccessMsgIsDisplayed();
-    uploadInvoicedOrdersPage.verifySuccessUploadNewFileIsDisplayed();
     pause3s();
   }
 
@@ -43,35 +37,48 @@ public class UploadInvoicedOrdersSteps extends AbstractSteps {
     trackingIds = resolveValues(trackingIds);
     File csvFile = StandardTestUtils.createFile("upload.csv", String.join("\n", trackingIds));
     NvLogger.info("Path of the created file : " + csvFile.getAbsolutePath());
-    uploadInvoicedOrdersPage.uploadInvoicedOrdersDialog.uploadFile(csvFile);
+    uploadInvoicedOrdersPage.uploadFile(csvFile);
     takesScreenshot();
-  }
-
-  @And("Operator clicks on Upload New File Button")
-  public void operatorClicksOnUploadNewFileButton() {
-    uploadInvoicedOrdersPage.uploadNewFileButton.click();
-    uploadInvoicedOrdersPage.uploadNewCsvDialog.uploadNewFile.click();
-    uploadInvoicedOrdersPage.waitUntilPageLoaded();
   }
 
   @Then("Operator uploads a PDF and verifies that any other file except csv is not allowed")
   public void operatorUploadsAPDFAndVerifiesThatAnyOtherFileExceptCsvIsNotAllowed() {
     String pdfFileName = "invalid-upload.pdf";
     File pdfFile = StandardTestUtils.createFile(pdfFileName, "TEST");
-    uploadInvoicedOrdersPage.uploadInvoicedOrdersDialog.chooseButton.setValue(pdfFile);
-    String actualErrorMsg = uploadInvoicedOrdersPage.getToastTopText();
-    String expectedToastText = "\"" + pdfFileName + "\" is not allowed.";
-    Assertions.assertThat(actualErrorMsg).as("Check error message").isEqualTo(expectedToastText);
+    uploadInvoicedOrdersPage.browseFilesInput.sendKeys(pdfFile);
+    String actualErrorMsg = uploadInvoicedOrdersPage.getErrorMsg();
+    String actualErrorDescription = uploadInvoicedOrdersPage.getErrorMsgDescription();
+    Assertions.assertThat(actualErrorMsg).as("Error message title is correct")
+        .isEqualTo("Error uploading file");
+    Assertions.assertThat(actualErrorDescription).as("Error message description is correct")
+        .isEqualTo("Error: Invalid file type");
+
   }
 
   @Then("Operator uploads an invalid CSV and verifies error message")
   public void operatorUploadsAnInvalidCSVAndVerifiesErrorMessage() {
     String csvFileName = "upload.csv";
     File csvFile = StandardTestUtils.createFile(csvFileName, "TEST1 , TEST2");
-    uploadInvoicedOrdersPage.uploadInvoicedOrdersDialog.uploadFile(csvFile);
-    String actualErrorMsg = uploadInvoicedOrdersPage.getToastTopText();
-    String expectedToastText = "Error parsing csv";
-    Assertions.assertThat(actualErrorMsg).as("Check error message").isEqualTo(expectedToastText);
+    uploadInvoicedOrdersPage.uploadFile(csvFile);
+    String actualErrorMsg = uploadInvoicedOrdersPage.getErrorMsg();
+    String actualErrorDescription = uploadInvoicedOrdersPage.getErrorMsgDescription();
+    Assertions.assertThat(actualErrorMsg).as("Error message title is correct")
+        .isEqualTo("Error uploading file");
+    Assertions.assertThat(actualErrorDescription).as("Error message description is correct")
+        .isEqualTo("Error: Invalid CSV file");
+
+  }
+
+
+  @And("Operator upload a CSV file without extension with below order ids and verify success message")
+  public void operatorUploadACSVFileWithoutExtensionWithBelowOrderIdsAndVerifySuccessMsg(
+      List<String> trackingIds) {
+    trackingIds = resolveValues(trackingIds);
+    File csvFile = StandardTestUtils.createFile("uploadfile", String.join("\n", trackingIds));
+    NvLogger.info("Path of the created file : " + csvFile.getAbsolutePath());
+    uploadInvoicedOrdersPage.uploadFile(csvFile);
+    uploadInvoicedOrdersPage.verifySuccessMsgIsDisplayed();
+    takesScreenshot();
   }
 
   @And("Operator clicks Download sample CSV template button on the Upload Invoiced Orders Page")
@@ -85,4 +92,11 @@ public class UploadInvoicedOrdersSteps extends AbstractSteps {
     String expectedString = String.join("\n", trackingIds);
     uploadInvoicedOrdersPage.verifyCsvFileDownloadedSuccessfully(expectedString);
   }
+
+  @When("Upload Invoiced Orders page is loaded")
+  public void uploadInvoicedOrdersPageIsLoaded() {
+    uploadInvoicedOrdersPage.switchToIframe();
+    uploadInvoicedOrdersPage.waitUntilLoaded();
+  }
+
 }
