521cfa24290fac59e1167f7548227872ae99ad33
Andres Setiawan <andressetiawan8@gmail.com>
2023-05-03 00:07:24 +0700

    Fix and refactor vehicle type management page scenarios

--- src/test/java/co/nvqa/operator_v2/cucumber/glue/VehicleTypeManagementSteps.java
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/VehicleTypeManagementSteps.java
@@ -1,11 +1,18 @@
 package co.nvqa.operator_v2.cucumber.glue;
 
 import co.nvqa.common.utils.StandardTestUtils;
+import co.nvqa.operator_v2.model.DriverTypeParams;
 import co.nvqa.operator_v2.model.VehicleType;
+import co.nvqa.operator_v2.selenium.page.AntTableV3;
 import co.nvqa.operator_v2.selenium.page.VehicleTypeManagementPage;
+import com.google.common.collect.ImmutableMap;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.guice.ScenarioScoped;
+import java.util.Map;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
 
 /**
  * @author Tristania Siagian
@@ -13,30 +20,41 @@ import io.cucumber.guice.ScenarioScoped;
 @ScenarioScoped
 public class VehicleTypeManagementSteps extends AbstractSteps {
 
-  private VehicleTypeManagementPage vehicleTypeManagementPage;
+  private VehicleTypeManagementPage vtmPage;
 
   public VehicleTypeManagementSteps() {
   }
 
   @Override
   public void init() {
-    vehicleTypeManagementPage = new VehicleTypeManagementPage(getWebDriver());
+    vtmPage = new VehicleTypeManagementPage(getWebDriver());
   }
 
   @When("^Operator create new Vehicle Type$")
   public void operatorCreateNewVehicleType() {
-    String vehicleTypeName = "VT-" + StandardTestUtils.generateDateUniqueString();
     VehicleType vehicleType = new VehicleType();
+    String vehicleTypeName = "VT-" + StandardTestUtils.generateDateUniqueString();
     vehicleType.setName(vehicleTypeName);
-    vehicleTypeManagementPage.addNewVehicleType(vehicleType);
-    put(KEY_CREATED_VEHICLE_TYPE, vehicleType);
+    vtmPage.inFrame(() -> {
+      vtmPage.addNewVehicleType(vehicleType);
+      put(KEY_CREATED_VEHICLE_TYPE, vehicleType);
+    });
   }
 
   @Then("Operator verify vehicle type")
   public void operatorVerifyNewVehicleIsCreated() {
     VehicleType vehicleType = get(KEY_CREATED_VEHICLE_TYPE);
-    vehicleTypeManagementPage.verifyVehicleType(vehicleType);
-    takesScreenshot();
+    vtmPage.refreshPage();
+    vtmPage.inFrame(() -> {
+      boolean isSearchBarDisplayed = vtmPage.seachBar.isDisplayed();
+      while (!isSearchBarDisplayed) {
+        pause(500);
+        isSearchBarDisplayed = vtmPage.seachBar.isDisplayed();
+      }
+      vtmPage.seachBar.sendKeys(vehicleType.getName());
+      vtmPage.verifyVehicleType(vehicleType);
+      takesScreenshot();
+    });
   }
 
   @When("Operator edit the vehicle type name")
@@ -44,8 +62,11 @@ public class VehicleTypeManagementSteps extends AbstractSteps {
     VehicleType vehicleType = get(KEY_CREATED_VEHICLE_TYPE);
     String oldName = vehicleType.getName();
     String newName = oldName + " [EDITED]";
-    vehicleTypeManagementPage.editVehicleType(oldName, newName);
-    VehicleType editedVehicleType = new VehicleType(vehicleType);
+    vtmPage.inFrame(() -> {
+      vtmPage.editVehicleType(oldName, newName);
+    });
+    VehicleType editedVehicleType = new VehicleType();
+    editedVehicleType.setId(vehicleType.getId());
     editedVehicleType.setName(newName);
     put(KEY_CREATED_VEHICLE_TYPE_EDITED, editedVehicleType);
   }
@@ -53,8 +74,10 @@ public class VehicleTypeManagementSteps extends AbstractSteps {
   @Then("Operator verify the edited vehicle type name is existed")
   public void verifyEditedVehicleType() {
     VehicleType vehicleType = get(KEY_CREATED_VEHICLE_TYPE_EDITED);
-    vehicleTypeManagementPage.verifyVehicleType(vehicleType);
-    takesScreenshot();
+    vtmPage.inFrame(() -> {
+      vtmPage.verifyVehicleType(vehicleType);
+      takesScreenshot();
+    });
   }
 
   @When("Operator delete the vehicle type name")
@@ -62,7 +85,10 @@ public class VehicleTypeManagementSteps extends AbstractSteps {
     VehicleType vehicleType =
         containsKey(KEY_CREATED_VEHICLE_TYPE_EDITED) ? get(KEY_CREATED_VEHICLE_TYPE_EDITED)
             : get(KEY_CREATED_VEHICLE_TYPE);
-    vehicleTypeManagementPage.deleteVehicleType(vehicleType.getName());
+    vtmPage.inFrame(() -> {
+      vtmPage.deleteVehicleType(vehicleType.getName());
+      takesScreenshot();
+    });
   }
 
   @Then("Operator verify vehicle type name is deleted")
@@ -70,13 +96,17 @@ public class VehicleTypeManagementSteps extends AbstractSteps {
     VehicleType vehicleType =
         containsKey(KEY_CREATED_VEHICLE_TYPE_EDITED) ? get(KEY_CREATED_VEHICLE_TYPE_EDITED)
             : get(KEY_CREATED_VEHICLE_TYPE);
-    vehicleTypeManagementPage.verifyVehicleTypeNotExist(vehicleType.getName());
-    takesScreenshot();
+    vtmPage.inFrame(() -> {
+      vtmPage.verifyVehicleTypeNotExist(vehicleType.getName());
+      takesScreenshot();
+    });
   }
 
   @When("Operator click on download CSV file button")
   public void downloadCSVFile() {
-    vehicleTypeManagementPage.csvDownload();
+    vtmPage.inFrame(() -> {
+      vtmPage.csvDownload();
+    });
   }
 
   @Then("Operator verify the CSV file")
@@ -84,7 +114,26 @@ public class VehicleTypeManagementSteps extends AbstractSteps {
     VehicleType vehicleType =
         containsKey(KEY_CREATED_VEHICLE_TYPE_EDITED) ? get(KEY_CREATED_VEHICLE_TYPE_EDITED)
             : get(KEY_CREATED_VEHICLE_TYPE);
-    vehicleTypeManagementPage.csvDownloadSuccessful(vehicleType.getName());
-    takesScreenshot();
+    vtmPage.inFrame(() -> {
+      vtmPage.csvDownloadSuccessful(vehicleType.getName());
+      takesScreenshot();
+    });
+  }
+
+  @And("^Operator gets new vehicle type on Vehicle type management page")
+  public void operatorGetsNewVehicleTypeOnVehicleTypeManagementPage() {
+    VehicleType vehicleType = get(KEY_CREATED_VEHICLE_TYPE);
+    vtmPage.refreshPage();
+    vtmPage.inFrame(() -> {
+      boolean isSearchBarDisplayed = vtmPage.seachBar.isDisplayed();
+      while (!isSearchBarDisplayed) {
+        pause(500);
+        isSearchBarDisplayed = vtmPage.seachBar.isDisplayed();
+      }
+      vtmPage.seachBar.sendKeys(vehicleType.getName());
+      Map<String, String> vehicleTypeData = vtmPage.vehicleTypeTable.readRow(1);
+      vehicleType.setId(Long.parseLong(vehicleTypeData.get("vehicleId")));
+      put(KEY_CREATED_VEHICLE_TYPE, vehicleType);
+    });
   }
 }
--- src/test/java/co/nvqa/operator_v2/selenium/page/VehicleTypeManagementPage.java
+++ src/test/java/co/nvqa/operator_v2/selenium/page/VehicleTypeManagementPage.java
@@ -1,70 +1,154 @@
 package co.nvqa.operator_v2.selenium.page;
 
+import co.nvqa.operator_v2.model.DriverTypeParams;
 import co.nvqa.operator_v2.model.VehicleType;
+import co.nvqa.operator_v2.selenium.elements.Button;
+import co.nvqa.operator_v2.selenium.elements.PageElement;
+import co.nvqa.operator_v2.selenium.elements.TextBox;
+import co.nvqa.operator_v2.selenium.page.DriverTypeManagementPage.DriverTypesTable;
+import com.google.common.collect.ImmutableMap;
+import io.cucumber.java.en_old.Ac;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.logging.Logger;
 import org.assertj.core.api.Assertions;
+import org.junit.platform.commons.util.StringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 /**
  * @author Tristania Siagian
  */
 @SuppressWarnings("WeakerAccess")
-public class VehicleTypeManagementPage extends OperatorV2SimplePage {
+public class VehicleTypeManagementPage extends SimpleReactPage {
 
+  public VehicleTypeTable vehicleTypeTable;
   private static final String NG_REPEAT = "vehicleType in $data";
-  private static final String CSV_FILENAME = "vehicle_types.csv";
-
+  private static final String CSV_FILENAME = "vehicle-types.csv";
   private static final String COLUMN_DATA_TITLE_NAME = "'commons.name' | translate";
-  private static final String ACTION_BUTTON_EDIT = "Edit";
-  private static final String ACTION_BUTTON_DEL = "Delete";
+  private static final String ACTION_BUTTON_EDIT = "update";
+  private static final String ACTION_BUTTON_DEL = "delete";
+
+  @FindBy(xpath = "//button[contains(@class,'ant-btn') and@type='button'][span[text() = 'Create Vehicle Type']]")
+  public Button btnCreateVehicleType;
+  @FindBy(xpath = "//button[contains(@class,'ant-btn') and@type='button' and text() = 'Download CSV File']")
+  public Button btnDownloadCSVFile;
+
+  @FindBy(xpath = "//button[contains(@class,'ant-btn') and@type='button'][span[text() = 'Add']]")
+  public Button btnAddVehicleType;
+
+  @FindBy(xpath = "//button[contains(@class,'ant-btn') and@type='button'][span[text() = 'Update']]")
+  public Button btnUpdateVehicleType;
+  @FindBy(xpath = "//button[contains(@class,'ant-btn') and@type='button'][span[text() = 'Delete']]")
+  public Button btnDeleteVehicleType;
+
+  @FindBy(xpath = "//div[@class='ant-modal-content'][div[div[text() = 'Add Vehicle Type']]]")
+  public PageElement addVehicleTypeModal;
+
+  @FindBy(xpath = "//div[@class='ant-modal-content'][div[div[text() = 'Update Vehicle Type']]]")
+  public PageElement updateVehicleTypeModal;
+
+  @FindBy(xpath = "//div[@class='ant-modal-content'][div[div[text() = 'Confirm delete']]]")
+  public PageElement deleteVehicleTypeModal;
 
   @FindBy(css = "md-dialog")
   public ConfirmDeleteDialog confirmDeleteDialog;
 
+  @FindBy(xpath = "//input[contains(@data-testid,'search-bar') and @placeholder = 'Search']")
+  public PageElement seachBar;
+
+  @FindBy(xpath = "//input[@id='name' and @class='ant-input']")
+  public PageElement txtFieldInput;
+
   public VehicleTypeManagementPage(WebDriver webDriver) {
     super(webDriver);
+    vehicleTypeTable = new VehicleTypeTable(webDriver);
+  }
+
+  public static class VehicleTypeTable extends AntTableV3<DriverTypeParams> {
+
+    public static final String COLUMN_ID = "vehicleId";
+    public static final String COLUMN_NAME = "vehicleTypeName";
+
+    public static final String ACTION_UPDATE = "update";
+    public static final String ACTION_DELETE = "delete";
+
+    public VehicleTypeTable(WebDriver webDriver) {
+      super(webDriver);
+      setColumnLocators(ImmutableMap.<String, String>builder()
+          .put(COLUMN_ID, "1")
+          .put(COLUMN_NAME, "2")
+          .build()
+      );
+      setActionButtonsLocators(ImmutableMap
+          .of(ACTION_UPDATE, "1", ACTION_DELETE, "2"));
+      setEntityClass(DriverTypeParams.class);
+    }
   }
 
   public void addNewVehicleType(VehicleType vehicleType) {
-    clickNvIconTextButtonByName("Add Vehicle Type");
-    waitUntilVisibilityOfElementLocated("//md-dialog[contains(@class, 'vehicle-type-add')]");
-    sendKeysById("name", vehicleType.getName());
-    clickNvButtonSaveByNameAndWaitUntilDone("Submit");
-    pause5s(); //This pause is used to wait until the cache is synced to all node. Sometimes we got an error that says the new Vehicle Type is not found.
+    vehicleTypeTable.waitUntilPageLoaded();
+    btnCreateVehicleType.click();
+    addVehicleTypeModal.waitUntilVisible();
+    txtFieldInput.sendKeys(vehicleType.getName());
+    btnAddVehicleType.click();
+    addVehicleTypeModal.waitUntilInvisible();
   }
 
   public void verifyVehicleType(VehicleType vehicleType) {
-    searchTable(vehicleType.getName());
-    String actualVehicleTypeName = getTextOnTable(1, COLUMN_DATA_TITLE_NAME);
-    Assertions.assertThat(actualVehicleTypeName).as("Vehicle Type name")
-        .isEqualTo(vehicleType.getName());
+    clearWebField(seachBar.getWebElement());
+    seachBar.sendKeys(vehicleType.getName());
+
+    Map<String, String> actualData = vehicleTypeTable.readRow(1);
+    Boolean isVehicleTypeNameMatch = vehicleType.getName()
+        .equals(actualData.get(VehicleTypeTable.COLUMN_NAME));
+    Boolean isVehicleIdMatch = vehicleType.getId()
+        .equals(Long.parseLong(actualData.get(VehicleTypeTable.COLUMN_ID)));
+
+    Assertions.assertThat(isVehicleTypeNameMatch).as("Vehicle Type Name is not match")
+        .isTrue();
+    Assertions.assertThat(isVehicleIdMatch).as("Vehicle ID is not match")
+        .isTrue();
   }
 
   public void verifyVehicleTypeNotExist(String expectedVehicleTypeName) {
-    waitUntilInvisibilityOfElementLocated(String.format("//tr[@ng-repeat='%s'][1]", NG_REPEAT));
-    searchTable(expectedVehicleTypeName);
+    clearWebField(seachBar.getWebElement());
+    seachBar.sendKeys(expectedVehicleTypeName);
     boolean isTableEmpty = isTableEmpty();
     Assertions.assertThat(isTableEmpty).as("Vehicle name still exist on table.").isTrue();
   }
 
   public void editVehicleType(String oldName, String newName) {
-    searchTable(oldName);
+    if (seachBar.getValue().isBlank() || seachBar.getValue().isEmpty()) {
+      seachBar.sendKeys(oldName);
+    }
     clickActionButtonOnTable(1, ACTION_BUTTON_EDIT);
-    waitUntilVisibilityOfElementLocated("//md-dialog[contains(@class, 'vehicle-type-edit')]");
-    sendKeysById("name", newName);
-    clickNvButtonSaveByNameAndWaitUntilDone("Submit");
-    pause5s(); //This pause is used to wait until the cache is synced to all node. Sometimes we got an error that says the new Vehicle Type is not found.
+    updateVehicleTypeModal.waitUntilVisible();
+    if (!(txtFieldInput.getValue().isBlank() || txtFieldInput.getValue().isEmpty())) {
+      clearWebField(txtFieldInput.getWebElement());
+      txtFieldInput.sendKeys(newName);
+    }
+    btnUpdateVehicleType.moveAndClick();
+    updateVehicleTypeModal.waitUntilInvisible();
   }
 
   public void deleteVehicleType(String name) {
-    searchTable(name);
+    if (seachBar.getValue().isBlank() || seachBar.getValue().isEmpty()) {
+      seachBar.sendKeys(name);
+    }
     clickActionButtonOnTable(1, ACTION_BUTTON_DEL);
-    confirmDeleteDialog.confirmDelete();
-    pause5s(); //This pause is used to wait until the cache is synced to all node. Sometimes we got an error that says the new Vehicle Type is not found.
+    deleteVehicleTypeModal.waitUntilVisible();
+    btnDeleteVehicleType.moveAndClick();
+    deleteVehicleTypeModal.waitUntilInvisible();
   }
 
   public void csvDownload() {
-    clickNvApiTextButtonByName("Download CSV File");
+    vehicleTypeTable.waitUntilPageLoaded();
+    btnDownloadCSVFile.click();
     pause5s(); //This pause is used to wait until the cache is synced to all node. Sometimes we got an error that says the new Vehicle Type is not found.
   }
 
@@ -73,7 +157,9 @@ public class VehicleTypeManagementPage extends OperatorV2SimplePage {
   }
 
   public boolean isTableEmpty() {
-    return !isElementExistFast(String.format("//tr[@ng-repeat='%s'][1]", NG_REPEAT));
+    List<WebElement> elements = findElementsBy(By.xpath(
+        "//tr[contains(@class,'ant-table-row')][td[contains(@class, 'ant-table-cell')]]"));
+    return elements.size() == 0;
   }
 
   public String getTextOnTable(int rowNumber, String columnDataTitle) {
@@ -81,6 +167,17 @@ public class VehicleTypeManagementPage extends OperatorV2SimplePage {
   }
 
   public void clickActionButtonOnTable(int rowNumber, String actionButtonName) {
-    clickActionButtonOnTableWithNgRepeat(rowNumber, actionButtonName, NG_REPEAT);
+//    clickActionButtonOnTableWithNgRepeat(rowNumber, actionButtonName, NG_REPEAT);
+    String updateBtnXpath = f(
+        "//button[@type='button' and @data-testid='%s-button' and contains(@class,'ant-btn')]",
+        actionButtonName);
+    WebElement btnTable = findElementsBy(By.xpath(updateBtnXpath)).get(rowNumber - 1);
+    if (!btnTable.isDisplayed()) {
+      Assertions.assertThat(btnTable.isDisplayed())
+          .as("Update button not found in table")
+          .isFalse();
+    } else {
+      btnTable.click();
+    }
   }
 }
