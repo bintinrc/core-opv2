ce0a6103530ab07c28209d1a20403fe9c2f22cea
Mohammed <mohammed.hmeidan@ninjavan.co>
2023-06-19 08:52:09 +0300

    .

--- src/test/java/co/nvqa/operator_v2/cucumber/glue/PickupAppointmentJobStepsV2.java
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/PickupAppointmentJobStepsV2.java
@@ -308,7 +308,8 @@ public class PickupAppointmentJobStepsV2 extends AbstractSteps {
   @Then("Verify that csv file is downloaded in pick up job page with filename for Job Id")
   public void verifyThatCsvFileIsDownloadedWithFilename(Map<String, String> dataTable) {
     Map<String, String> data = resolveKeyValues(dataTable);
-    String newFilename = "pop-file-id-" + data.get("Job Id") + ".csv";
+    String newFilename = "pop-file-id-JobId.csv";
+    newFilename.replaceAll("JobId", data.get("Job Id"));
     Runnable verifyDownloadedFilename = () -> {
       pickupAppointmentJobPage.inFrame(page -> {
         page.viewJobDetailModal.verifyThatCsvFileIsDownloadedWithFilename(newFilename);
