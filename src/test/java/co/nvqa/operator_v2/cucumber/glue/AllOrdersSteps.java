a3103b15491418fb03cc817855347395f7417813
Anderson <anderson.susanto@ninjavan.co>
2023-07-11 16:45:10 +0700

    remove old method for RTS orders

--- src/test/java/co/nvqa/operator_v2/cucumber/glue/AllOrdersSteps.java
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/AllOrdersSteps.java
@@ -528,15 +528,7 @@ public class AllOrdersSteps extends AbstractSteps {
   public void operatorVerifiesAllOrdersPageIsDispalyed() {
     allOrdersPage.verifyItsCurrentPage();
   }
-
-  @When("^Operator RTS multiple orders on next day on All Orders page$")
-  public void operatorRtsMultipleOrdersOnNextDayOnAllOrdersPage() {
-    List<Order> listOfCreatedOrder = get(KEY_LIST_OF_CREATED_ORDER);
-    List<String> listOfTrackingIds = listOfCreatedOrder.stream().map(Order::getTrackingId)
-        .collect(Collectors.toList());
-    allOrdersPage.rtsMultipleOrderNextDay(listOfTrackingIds);
-  }
-
+  
   @When("Operator RTS multiple orders on next day on All Orders Page:")
   public void operatorRtsOrdersOnNextDayOnAllOrdersPage(List<String> listOfTrackingIds) {
     List<String> resolveListOfTrackingIds = resolveValues(listOfTrackingIds);
