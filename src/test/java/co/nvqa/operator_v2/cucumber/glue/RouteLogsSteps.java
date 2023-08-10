bfda22843068571a2c13254ce35de6347b63fe27
Anderson <anderson.susanto@ninjavan.co>
2023-08-10 10:14:48 +0700

    no message

--- src/test/java/co/nvqa/operator_v2/cucumber/glue/RouteLogsSteps.java
+++ src/test/java/co/nvqa/operator_v2/cucumber/glue/RouteLogsSteps.java
@@ -1072,7 +1072,7 @@ public class RouteLogsSteps extends AbstractSteps {
         try {
           toastInfo = routeLogsPage.noticeNotifications.stream().filter(toast -> {
             String actualTop = toast.message.getNormalizedText();
-            toast.moveToElement();
+            toast.message.moveToElement();
             LOGGER.info("Found notification: " + actualTop);
             String value = finalData.get("top");
             if (StringUtils.isNotBlank(value)) {
@@ -1089,7 +1089,7 @@ public class RouteLogsSteps extends AbstractSteps {
             value = finalData.get("bottom");
             if (StringUtils.isNotBlank(value)) {
               String actual = toast.description.getNormalizedText();
-              toast.moveToElement();
+              toast.description.moveToElement();
               LOGGER.info("Found description: " + actual);
               if (value.startsWith("^")) {
                 return actual.matches(value);
