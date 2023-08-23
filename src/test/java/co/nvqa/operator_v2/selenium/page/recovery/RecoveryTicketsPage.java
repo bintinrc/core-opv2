68542c295e632cf1474c5f5bde18d467cfe5aa54
Batool Sammoudi <batool.sammoudi@ninjavan.co>
2023-08-23 10:39:24 +0300

    remove unused import

--- src/test/java/co/nvqa/operator_v2/selenium/page/recovery/RecoveryTicketsPage.java
+++ src/test/java/co/nvqa/operator_v2/selenium/page/recovery/RecoveryTicketsPage.java
@@ -25,7 +25,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 
 public class RecoveryTicketsPage extends SimpleReactPage<RecoveryTicketsPage> {
 
